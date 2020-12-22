dd7251209574a4991d14964f8deeaa771dd6e396
Verveine <samuelpitch@hotmail.fr>
2020-12-22 06:20:11 +0100

    Add files via upload

---
+++ Alchemist/OrchidModAlchemistEquipable.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace OrchidMod.Alchemist
+{
+	public abstract class OrchidModAlchemistEquipable : ModItem
+	{
+		public virtual void SafeSetDefaults() {}
+
+		public sealed override void SetDefaults() {
+			SafeSetDefaults();
+		}
+		
+		public override void ModifyWeaponDamage(Player player, ref float add, ref float mult, ref float flat) {
+			mult *= player.GetModPlayer<OrchidModPlayer>().gamblerDamage;
+		}
+		
+		public override void GetWeaponCrit(Player player, ref int crit) {
+			crit += player.GetModPlayer<OrchidModPlayer>().gamblerCrit;
+		}
+		
+		public override void ModifyTooltips(List<TooltipLine> tooltips) {
+			TooltipLine tt = tooltips.FirstOrDefault(x => x.Name == "Damage" && x.mod == "Terraria");
+			if (tt != null) {
+				string[] splitText = tt.text.Split(' ');
+				string damageValue = splitText.First();
+				string damageWord = splitText.Last();
+				tt.text = damageValue + " chemical " + damageWord;
+			}
+			
+			Mod thoriumMod = ModLoader.GetMod("ThoriumMod");
+			if (thoriumMod != null) {
+				tooltips.Insert(1, new TooltipLine(mod, "ClassTag", "-Alchemist Class-")
+				{
+					overrideColor = new Color(155, 255, 55)
+				});
+			}
+		}
+	}
+}
