6be8ff7550967aa8bf2ecab032e041ba2c5db490
Verveine <samuelpitch@hotmail.fr>
2021-03-23 18:04:38 +0100

    Fixed a dummy card bug with biome cards

--- Gambler/GamblerDummy.cs
+++ Gambler/GamblerDummy.cs
@@ -64,7 +64,7 @@ namespace OrchidMod.Gambler
 			OrchidModPlayer modPlayer = player.GetModPlayer<OrchidModPlayer>();
 			modPlayer.gamblerAttackInHand = true;
 			if (Main.mouseLeft) {
-				GamblerAttackHelper.shootBonusProjectiles(player, player.Center, modPlayer.gamblerCardCurrent.type, true);
+				GamblerAttackHelper.shootBonusProjectiles(player, player.Center, modPlayer.gamblerCardDummy.type, true);
 			}
 		}
 		
