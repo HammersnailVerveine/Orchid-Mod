f516a08e0ede69761e3c8a8fad78260df0c15560
S-Pladison <36926642+S-Pladison@users.noreply.github.com>
2021-04-04 20:49:33 +0300

    Add files via upload

---
+++ Dusts/StrangeSmokeDust.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Terraria;
+
+namespace OrchidMod.Dusts
+{
+	public class StrangeSmokeDust : ModDust
+	{
+		public override void OnSpawn(Dust dust)
+		{
+			dust.frame = new Rectangle(0, Main.rand.Next(0, 5) * 10, 10, 10);
+		}
+
+		public override bool Update(Dust dust)
+		{
+			if (dust.scale < 0.15f) dust.active = false;
+
+			dust.rotation += Math.Sign(Main.rand.Next(-1, 0)) * 0.01f;
+			dust.position += dust.velocity;
+
+			dust.scale *= 0.95f;
+			dust.velocity *= 0.95f;
+
+			return false;
+		}
+	}
+}
