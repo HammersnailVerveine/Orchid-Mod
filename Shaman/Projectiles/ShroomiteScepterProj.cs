8122420e1aa6e270d66100e481b2f8132ef5df77
S-Pladison <36926642+S-Pladison@users.noreply.github.com>
2021-04-04 20:56:06 +0300

    Add files via upload

--- Shaman/Projectiles/ShroomiteScepterProj.cs
+++ Shaman/Projectiles/ShroomiteScepterProj.cs
@@ -6,13 +6,16 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using OrchidMod.Shaman;
 using System.Collections.Generic;
+using OrchidMod.Effects;
 
 namespace OrchidMod.Shaman.Projectiles
 {
-    public class ShroomiteScepterProj : OrchidModShamanProjectile
-    {
-		private double dustVal = 0;
-		private int dustSize = 20;
+	public class ShroomiteScepterProj : OrchidModShamanProjectile
+	{
+		public Texture2D zoneTexture;
+
+		public ref float Radius => ref projectile.ai[0];
+		public ref float NbBonds => ref projectile.ai[1];
 
 		public override void SetStaticDefaults()
 		{
@@ -20,125 +23,119 @@ namespace OrchidMod.Shaman.Projectiles
 		}
 
 		public override void SafeSetDefaults()
-        {
-            projectile.width = 16;
-            projectile.height = 16;
-            projectile.friendly = false;
-            projectile.aiStyle = 1;
-			projectile.timeLeft = 1800;	
-			projectile.scale = 1f;
-
-			projectile.aiStyle = 53;
-        }
-
-		private void OldAI()
 		{
-			Player player = Main.player[projectile.owner];
-			OrchidModPlayer modPlayer = player.GetModPlayer<OrchidModPlayer>();
-			int range = 200 + (int)projectile.ai[1] * 50;
+			projectile.width = 16;
+			projectile.height = 16;
+			projectile.friendly = false;
+			projectile.timeLeft = 1800;
+			projectile.scale = 1f;
+			projectile.hide = true; // DrawBehind()
 
-			if (projectile.velocity.Y < 1f)
-			{
-				spawnDust(172, range);
-				if (projectile.timeLeft % 2 == 0)
-				{
-					spawnDust(172, dustSize);
-				}
-
-				if (projectile.timeLeft % 120 == 0)
-				{
-					for (int i = 0; i < Main.npc.Length; i++)
-					{
-						NPC target = Main.npc[i];
-						Vector2 center = new Vector2(projectile.position.X + projectile.width * 0.5f, projectile.position.Y + projectile.height * 0.5f);
-						float offsetX = target.Center.X - center.X;
-						float offsetY = target.Center.Y - center.Y;
-						float distance = (float)Math.Sqrt(offsetX * offsetX + offsetY * offsetY);
-						if (!target.friendly && distance < 500f && projectile.position.X - range + 30 < target.position.X + target.width && projectile.position.X + projectile.width + range + 30 > target.position.X
-						&& projectile.position.Y - range + 30 < target.position.Y + target.height && projectile.position.Y + projectile.height + range + 30 > target.position.Y)
-						{
-							target.StrikeNPCNoInteraction(projectile.damage, 0f, 0);
-							if (projectile.ai[1] >= 3)
-							{
-								OrchidModGlobalNPC modTarget = target.GetGlobalNPC<OrchidModGlobalNPC>();
-								modTarget.shamanShroom = 300;
-								OrchidModShamanHelper.addShamanicEmpowerment(4, 4, player, modPlayer, mod);
-							}
-							target.netUpdate = true;
-						}
-					}
-				}
-			}
-
-			if (projectile.ai[1] == 5)
-			{
-				Vector2 center = new Vector2(projectile.position.X + projectile.width * 0.5f, projectile.position.Y + projectile.height * 0.5f);
-				float offsetX = player.Center.X - center.X;
-				float offsetY = player.Center.Y - center.Y;
-				float distance = (float)Math.Sqrt(offsetX * offsetX + offsetY * offsetY);
-				if (distance < 500f && projectile.position.X - range + 30 < player.position.X + player.width && projectile.position.X + projectile.width + range + 30 > player.position.X
-				&& projectile.position.Y - range + 30 < player.position.Y + player.height && projectile.position.Y + projectile.height + range + 30 > player.position.Y)
-				{
-					player.AddBuff(mod.BuffType("ShroomHeal"), 1);
-				}
-			}
-
-			this.dustVal++;
-			this.dustSize = this.dustSize < range ? this.dustSize + 1 : 20;
+			/*empowermentType = 4;
+			empowermentLevel = 4;*/
 		}
-		
-        public override void AI()
-        {
-			OldAI();
 
-			Color color = new Color(0.37f, 0.8f, 1f) * 0.5f;
+		public override void OnSpawn()
+		{
+			Radius = 200 + (int)NbBonds * 50;
+
+			zoneTexture = new Texture2D(Main.graphics.GraphicsDevice, (int)Radius * 2, (int)Radius * 2);
+		}
+
+		public override void AI()
+		{
+			Player owner = Main.player[projectile.owner];
+
+			projectile.velocity.X = 0f;
+			projectile.velocity.Y += +0.2f;
+			if (projectile.velocity.Y > 16f) projectile.velocity.Y = 16f;
+
+			// DPS Zone
+			if (projectile.timeLeft % 120 == 0)
+			{
+				foreach (var target in Main.npc)
+				{
+					if (!target.active || !target.CanBeChasedBy() || Vector2.Distance(projectile.Center, target.Center) > Radius) continue;
+
+					target.StrikeNPCNoInteraction(projectile.damage, 0f, 0);
+					if (NbBonds >= 3)
+					{
+						target.GetGlobalNPC<OrchidModGlobalNPC>().shamanShroom = 300;
+						OrchidModShamanHelper.addShamanicEmpowerment(4, 4, owner, owner.GetModPlayer<OrchidModPlayer>(), mod);
+					}
+					target.netUpdate = true;
+				}
+			}
+
+			// Buff Zone
+			if (NbBonds >= 5)
+			{
+				Player buffPlayer = Main.player[Main.myPlayer]; // I hope it works
+
+				if (Vector2.Distance(buffPlayer.Center, projectile.Center) <= Radius) buffPlayer.AddBuff(ModContent.BuffType<Buffs.ShroomHeal>(), 1);
+			}
+
+			Color color = new Color(36, 129, 234) * 0.4f;
 			Lighting.AddLight(projectile.Center, color.R / 255f, color.G / 255f, color.B / 255f);
+		}
+
+		public override void DrawBehind(int index, List<int> drawCacheProjsBehindNPCsAndTiles, List<int> drawCacheProjsBehindNPCs, List<int> drawCacheProjsBehindProjectiles, List<int> drawCacheProjsOverWiresUI)
+		{
+			drawCacheProjsBehindNPCs.Add(index);
 		}
 
 		public override bool OrchidPreDraw(SpriteBatch spriteBatch, Color lightColor)
 		{
-			Vector2 offset = new Vector2(-7, -20);
+			Vector2 offset = new Vector2(-7, -22);
+			Vector2 drawPosition = projectile.position - Main.screenPosition + projectile.Size * 0.5f;
 			Color color = Lighting.GetColor((int)(projectile.position.X / 16f), (int)(projectile.position.Y / 16f), Color.White);
-			SpriteEffects effect = projectile.spriteDirection == 1 ? SpriteEffects.None : SpriteEffects.FlipHorizontally;
+			SpriteEffects spriteEffect = projectile.spriteDirection == 1 ? SpriteEffects.None : SpriteEffects.FlipHorizontally;
 
-			spriteBatch.Draw(Main.projectileTexture[projectile.type], projectile.position - Main.screenPosition + projectile.Size * 0.5f + offset, null, color, projectile.rotation, projectile.Size * 0.5f, projectile.scale, effect, 0f);
-			spriteBatch.Draw(ModContent.GetTexture("OrchidMod/Glowmasks/ShroomiteScepterProj_Glowmask"), projectile.position - Main.screenPosition + projectile.Size * 0.5f + offset, null, new Color(250, 250, 250), projectile.rotation, projectile.Size * 0.5f, projectile.scale, effect, 0f);
+			// Light Effect
+			EffectsManager.SetSpriteBatchEffectSettings(spriteBatch, blendState: BlendState.Additive);
+			{
+				Texture2D radialGradient = EffectsManager.RadialGradientTexture;
+				spriteBatch.Draw(radialGradient, projectile.Center - Main.screenPosition + new Vector2(0f, projectile.gfxOffY) + new Vector2(0, -11), null, new Color(36, 129, 234) * 0.35f, 0f, radialGradient.Size() * 0.5f, 0.75f * projectile.scale, SpriteEffects.None, 0f);
+			}
+
+			// Aura
+			var effect = EffectsManager.ShroomiteZoneEffect;
+			EffectsManager.SetSpriteBatchEffectSettings(spriteBatch, effect, blendState: BlendState.Additive);
+			{
+				effect.Parameters["time"].SetValue(Main.GlobalTime);
+				effect.Parameters["radius"].SetValue(Radius);
+				effect.Parameters["thickness"].SetValue(2.5f);
+				effect.Parameters["color"].SetValue(new Vector4(73, 76, 219, 85) / 255f);
+
+				if (zoneTexture != null) spriteBatch.Draw(zoneTexture, drawPosition, null, Color.White, projectile.rotation, zoneTexture.Size() * 0.5f, 1f, SpriteEffects.None, 0f);
+			}
+			EffectsManager.SetSpriteBatchVanillaSettings(spriteBatch);
+
+			// Projectile and Glowmask
+			spriteBatch.Draw(Main.projectileTexture[projectile.type], drawPosition + offset, null, color, projectile.rotation, projectile.Size * 0.5f, projectile.scale, spriteEffect, 0f);
+			EffectsManager.SetSpriteBatchEffectSettings(spriteBatch, blendState: BlendState.Additive);
+			{
+				spriteBatch.Draw(ModContent.GetTexture("OrchidMod/Glowmasks/ShroomiteScepterProj_Glowmask"), drawPosition + offset, null, new Color(250, 250, 250, 100), projectile.rotation, projectile.Size * 0.5f, projectile.scale, spriteEffect, 0f);
+			}
+			EffectsManager.SetSpriteBatchVanillaSettings(spriteBatch);
 
 			return false; // Let's draw the projectile ourselves
 		}
 
 		public override bool OnTileCollide(Vector2 oldVelocity) => false;
-		
+
 		public override void Kill(int timeLeft)
-        {
-            for(int i=0; i<5; i++)
-            {
-                int dust = Dust.NewDust(projectile.position, projectile.width, projectile.height, 172);
-				Main.dust[dust].velocity = projectile.velocity / 2;
-				Main.dust[dust].noGravity = true;
-            }
-        }
-
-		public void spawnDust(int dustType, int distToCenter)
 		{
-			for (int i = 0; i < 3; i++)
+			zoneTexture = null;
+
+			for (int i = 0; i < 10; i++)
 			{
-				double deg = (4 * (42 + this.dustVal) + i * 120);
-				double rad = deg * (Math.PI / 180);
-
-				float posX = projectile.Center.X - (int)(Math.Cos(rad) * distToCenter) + projectile.velocity.X - 4;
-				float posY = projectile.Center.Y - (int)(Math.Sin(rad) * distToCenter) + projectile.velocity.Y - 4;
-
-				Vector2 dustPosition = new Vector2(posX, posY);
-
-				int index2 = Dust.NewDust(dustPosition, 1, 1, dustType, 0.0f, 0.0f, 0, new Color(), Main.rand.Next(30, 130) * 0.013f);
-
-				Main.dust[index2].velocity = projectile.velocity / 2;
-				Main.dust[index2].fadeIn = 1f;
-				Main.dust[index2].scale = projectile.velocity.X == 0 ? 1.5f : (float)Main.rand.Next(70, 110) * 0.013f;
-				Main.dust[index2].noGravity = true;
+				var dust = Main.dust[Dust.NewDust(projectile.position + new Vector2(-8, -26), Main.projectileTexture[projectile.type].Width, Main.projectileTexture[projectile.type].Height, ModContent.DustType<Dusts.StrangeSmokeDust>())];
+				dust.velocity = new Vector2(Main.rand.NextFloat(-0.5f, 0.5f), Main.rand.NextFloat(-0.25f, 0.75f));
+				dust.alpha = 200;
+				dust.scale = Main.rand.NextFloat(2f, 3.5f);
+				dust.rotation = Main.rand.NextFloat(-(float)Math.PI, (float)Math.PI);
 			}
 		}
-
 	}
 }
