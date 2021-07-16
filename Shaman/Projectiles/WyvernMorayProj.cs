2a580b23b73753fa6a92a5f5815bf798246e954e
S-Pladison <36926642+S-Pladison@users.noreply.github.com>
2021-07-16 19:55:08 +0300

    Add files via upload

--- Shaman/Projectiles/WyvernMorayProj.cs
+++ Shaman/Projectiles/WyvernMorayProj.cs
@@ -1,6 +1,7 @@
 using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using OrchidMod.Effects.Trails;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -9,80 +10,139 @@ namespace OrchidMod.Shaman.Projectiles
 {
 	public class WyvernMorayProj : OrchidModShamanProjectile
 	{
-		
+		public bool Improved { get => projectile.ai[1] == 1; set => projectile.ai[1] = value.ToInt(); }
+
+		private bool _death = false;
+		private float _deathProgress = 1f;
+		private Effects.Primitives.Trail _trail;
+
 		public override void SetStaticDefaults()
-        {
-            DisplayName.SetDefault("Wyvern Spit");
-        } 
-		
+		{
+			DisplayName.SetDefault("Wyvern Spit");
+
+			ProjectileID.Sets.TrailingMode[projectile.type] = 0;
+			ProjectileID.Sets.TrailCacheLength[projectile.type] = 15;
+		}
+
 		public override void SafeSetDefaults()
 		{
-            projectile.width = 14;
-            projectile.height = 14;
-            projectile.friendly = true;
-            projectile.aiStyle = 2;
+			projectile.width = 14;
+			projectile.height = 14;
+			projectile.aiStyle = 2;
+			projectile.friendly = true;
 			projectile.timeLeft = 150;
 			projectile.alpha = 255;
-            this.empowermentType = 2;
+			projectile.penetrate = -1;
+
+			this.empowermentType = 2;
 		}
-		
-        public override void AI()
-        {	
-		    if (Main.rand.Next(2) == 0)
-            {
-                int dust = Dust.NewDust(projectile.position, projectile.width, projectile.height, 15);
-				Main.dust[dust].noGravity = true;
-				Main.dust[dust].scale = 1.5f;
-				Main.dust[dust].velocity = projectile.velocity / 2;
-            }
-			
-		    if (Main.rand.Next(2) == 0)
-            {
-                int dust = Dust.NewDust(projectile.position, projectile.width, projectile.height, 16);
-				Main.dust[dust].noGravity = true;
-				Main.dust[dust].scale = 1.5f;
-				Main.dust[dust].velocity = projectile.velocity / 2;
-            }
-			
-			if (projectile.timeLeft % 5 == 0) spawnDustCircle(16, 10 + Main.rand.Next(10));
+
+		private readonly Color[] _effectColors = new Color[] { new Color(113, 187, 162), new Color(40, 116, 255) };
+
+		public Color GetCurrentColor() => _effectColors[Improved.ToInt()] * _deathProgress;
+
+		public override void OnSpawn()
+		{
+			_trail = new TriangularTrail(length: 16 * 13, width: (p) => 20 * (1 - p * 0.25f), color: (p) => GetCurrentColor() * (1 - p), effect: mod.GetEffect("Effects/WyvernMoray"))
+			{
+				MaxPoints = 35
+			};
+			_trail.SetEffectTexture(ModContent.GetTexture("OrchidMod/Effects/Textures/Trail_1"), 0);
+			_trail.SetEffectTexture(ModContent.GetTexture("OrchidMod/Effects/Textures/Cloud"), 1);
+			OrchidMod.Primitives?.CreateTrail(target: projectile, trail: _trail);
 		}
-		
-		public void spawnDustCircle(int dustType, int distToCenter) {
-			for (int i = 0 ; i < 20 ; i ++ ) {
-				double deg = (i * (36 + 5 - Main.rand.Next(10)));
-				double rad = deg * (Math.PI / 180);
-					 
-				float posX = projectile.Center.X - (int)(Math.Cos(rad) * distToCenter) - projectile.width/2 + 4;
-				float posY = projectile.Center.Y - (int)(Math.Sin(rad) * distToCenter) - projectile.height/2 + 4;
-					
-				Vector2 dustPosition = new Vector2(posX, posY);
-					
-				int index2 = Dust.NewDust(dustPosition, 1, 1, dustType, 0.0f, 0.0f, 0, new Color(), Main.rand.Next(30, 130) * 0.013f);
-					
-				Main.dust[index2].velocity = projectile.velocity / 2;
-				Main.dust[index2].fadeIn = 1f;
-				Main.dust[index2].scale = 1.5f;
-				Main.dust[index2].noGravity = true;
+
+		public override void ModifyHitNPC(NPC target, ref int damage, ref float knockback, ref bool crit, ref int hitDirection)
+		{
+			if (Improved) damage += damage;
+		}
+
+		public override void AI()
+		{
+			if (_death) this.DeathUpdate();
+		}
+
+		public override void PostDraw(SpriteBatch spriteBatch, Color lightColor)
+		{
+			Vector2 drawPos = projectile.Center - Main.screenPosition + new Vector2(0f, projectile.gfxOffY);
+			Texture2D texture;
+			Color color = GetCurrentColor();
+			Effects.EffectsManager.SetSpriteBatchEffectSettings(spriteBatch, blendState: BlendState.Additive);
+
+			// Trail
+			{
+				texture = Effects.EffectsManager.RadialGradientTexture;
+				for (int k = 1; k < projectile.oldPos.Length; k++)
+				{
+					float progress = ((float)(projectile.oldPos.Length - k) / (float)projectile.oldPos.Length);
+					Vector2 drawPosTrail = projectile.oldPos[k] - Main.screenPosition + projectile.Size * 0.5f + new Vector2(0f, projectile.gfxOffY);
+					spriteBatch.Draw(texture, drawPosTrail, null, color * progress, projectile.rotation, texture.Size() * 0.5f, projectile.scale * 0.4f * progress, SpriteEffects.None, 0f);
+				}
+				spriteBatch.Draw(texture, drawPos, null, color, projectile.velocity.ToRotation() + MathHelper.PiOver2, texture.Size() * 0.5f, projectile.scale * 0.6f, SpriteEffects.None, 0);
 			}
+
+			texture = ModContent.GetTexture("OrchidMod/Effects/Textures/AnemoTwirl");
+			spriteBatch.Draw(texture, drawPos, null, color * 0.8f, Main.GlobalTime * 5f, texture.Size() * 0.5f, projectile.scale * 0.3f, SpriteEffects.None, 0);
+
+			texture = ModContent.GetTexture("OrchidMod/Effects/Textures/Circle");
+			spriteBatch.Draw(texture, drawPos, null, color * _deathProgress, projectile.velocity.ToRotation() + MathHelper.PiOver2, texture.Size() * 0.5f, projectile.scale * 0.4f, SpriteEffects.None, 0);
+
+			texture = ModContent.GetTexture("OrchidMod/Effects/Textures/Hmmm");
+			spriteBatch.Draw(texture, drawPos + Vector2.Normalize(projectile.velocity) * 8f, null, color * MathHelper.SmoothStep(0, 1, projectile.velocity.Length() * 0.1f), projectile.velocity.ToRotation() + MathHelper.PiOver2, texture.Size() * 0.5f, projectile.scale * 0.4f, SpriteEffects.None, 0);
+
+			if (_death)
+			{
+				texture = ModContent.GetTexture("OrchidMod/Effects/Textures/Ring");
+				float progress = 1 - (float)Math.Pow(MathHelper.Lerp(0, 1, _deathProgress), 3);
+				color *= progress;
+				Vector2 origin = texture.Size() * 0.5f;
+				float scale = projectile.scale * progress;
+
+				spriteBatch.Draw(texture, drawPos, null, color * 0.6f, 0f, origin, scale, SpriteEffects.None, 0);
+				spriteBatch.Draw(texture, drawPos, null, color, 0f, origin, scale * 1.6f, SpriteEffects.None, 0);
+			}
+
+			Effects.EffectsManager.SetSpriteBatchVanillaSettings(spriteBatch);
 		}
-		
-		public override void Kill(int timeLeft)
-        {
-			Player player = Main.player[projectile.owner];
-			
-			spawnDustCircle(16, 40);
-			Main.PlaySound(2, (int)projectile.position.X, (int)projectile.position.Y, 34);
-			Projectile.NewProjectile(projectile.Center.X, projectile.Center.Y, 0f, 0f, mod.ProjectileType("WyvernMorayProjLingering"), (int)(projectile.damage * 0.8f), 0.0f, projectile.owner, 0.0f, 0.0f);
-			
-			for(int i=0; i<10; i++) {
-				int dustType = Main.rand.Next(2) == 0 ? 15 : 16;
-                int dust = Dust.NewDust(projectile.position, projectile.width, projectile.height, dustType);
-				Main.dust[dust].noGravity = true;
-				Main.dust[dust].velocity *= 5f;
-				Main.dust[dust].scale *= 1.5f;
-            }
-        }
-		
-		public override void SafeOnHitNPC(NPC target, int damage, float knockback, bool crit, Player player, OrchidModPlayer modPlayer) {}
+
+		public void DeathUpdate()
+		{
+			projectile.velocity = Vector2.Zero;
+			projectile.timeLeft = 2;
+
+			if (_deathProgress == 1f)
+			{
+				Main.PlaySound(SoundID.Item, (int)projectile.position.X, (int)projectile.position.Y, 34);
+
+				projectile.friendly = false;
+				projectile.tileCollide = false;
+				_trail.StartDissolving();
+
+				var proj = Main.projectile[Projectile.NewProjectile(projectile.Center.X, projectile.Center.Y, 0f, 0f, ModContent.ProjectileType<WyvernMorayProjLingering>(), (int)(projectile.damage * 0.6f), 0.0f, projectile.owner, 0.0f, 0.0f)];
+				if (proj.modProjectile is WyvernMorayProjLingering hehe)
+				{
+					hehe.effectColor = GetCurrentColor();
+					hehe.Improved = this.Improved;
+					proj.netUpdate = true;
+				}
+			}
+
+			_deathProgress -= 0.085f;
+
+			if (_deathProgress <= 0f) projectile.Kill();
+		}
+
+		public override bool OnTileCollide(Vector2 oldVelocity)
+		{
+			projectile.velocity = oldVelocity;
+
+			_death = true;
+			return false;
+		}
+
+		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+		{
+			_death = true;
+		}
 	}
 }
--- Shaman/Projectiles/WyvernMorayProjLingering.cs
+++ Shaman/Projectiles/WyvernMorayProjLingering.cs
@@ -7,35 +7,63 @@ using Terraria.ModLoader;
 
 namespace OrchidMod.Shaman.Projectiles
 {
-    public class WyvernMorayProjLingering : OrchidModShamanProjectile
-    {
-        public override void SafeSetDefaults()
-        {
-            projectile.width = 100;
-            projectile.height = 100;
-            projectile.friendly = true;
-            projectile.aiStyle = 0;
-			projectile.timeLeft = 120;
+	public class WyvernMorayProjLingering : OrchidModShamanProjectile
+	{
+		public bool Improved { get => projectile.ai[0] == 1; set => projectile.ai[0] = value.ToInt(); }
+		public float Opacity { get => projectile.ai[1]; set => projectile.ai[1] = value; }
+
+		public Color effectColor = new Color(113, 187, 162);
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Wyvern Dusts");
+		}
+
+		public override void SafeSetDefaults()
+		{
+			projectile.width = 100;
+			projectile.height = 100;
+			projectile.friendly = true;
+			projectile.aiStyle = 0;
+			projectile.timeLeft = 180;
 			projectile.scale = 1f;
 			projectile.alpha = 255;
 			projectile.penetrate = -1;
-            this.empowermentType = 2;
-        }
-		
-		public override void SetStaticDefaults()
-        {
-            DisplayName.SetDefault("Wyvern Dusts");
-        }
-		
-        public override void AI()
-        {
-			int dust2 = Dust.NewDust(projectile.position, projectile.width, projectile.height, Main.rand.Next(2) == 0 ? 15 : 16);
-			Main.dust[dust2].scale = 1.2f;
-			Main.dust[dust2].noGravity = true;
-			// Main.dust[dust2].velocity.X /= 3;
-			// Main.dust[dust2].velocity.Y *= 3;
+			projectile.tileCollide = false;
+
+			this.empowermentType = 2;
 		}
-		
-		public override void SafeOnHitNPC(NPC target, int damage, float knockback, bool crit, Player player, OrchidModPlayer modPlayer) {}
-    }
+
+		public override void ModifyHitNPC(NPC target, ref int damage, ref float knockback, ref bool crit, ref int hitDirection)
+		{
+			if (Improved) damage += damage;
+		}
+
+		public override void AI()
+		{
+			//this.Opacity = 1 - (float)Math.Pow(MathHelper.Lerp(0.0f, 1.0f, Math.Abs(1 - (projectile.timeLeft / 90f))), 5f);
+			this.Opacity = 1 - (float)Math.Pow(MathHelper.Lerp(0.0f, 1.0f, (1 - projectile.timeLeft / 180f)), 5f); // WTF!!!
+
+			Lighting.AddLight(projectile.Center, effectColor.ToVector3() * 0.4f * this.Opacity);
+		}
+
+		public override bool OrchidPreDraw(SpriteBatch spriteBatch, Color lightColor)
+		{
+			Vector2 drawPos = projectile.Center - Main.screenPosition + new Vector2(0f, projectile.gfxOffY);
+			Texture2D texture = Effects.EffectsManager.RadialGradientTexture;
+
+			Effect effect = mod.GetEffect("Effects/WyvernMorayLingering");
+			effect.Parameters["time"].SetValue(Main.GlobalTime * 0.1f + projectile.position.X * 2);
+			effect.Parameters["texture1"].SetValue(ModContent.GetTexture("OrchidMod/Effects/Textures/CloudNoise"));
+
+			Color color = Lighting.GetColor((int)projectile.Center.X / 16, (int)projectile.Center.Y / 16, effectColor * this.Opacity);
+			Vector2 origin = texture.Size() * 0.5f;
+			float scale = projectile.scale * 2.25f;
+
+			Effects.EffectsManager.SetSpriteBatchEffectSettings(spriteBatch, effect: effect, blendState: BlendState.Additive);
+			for (int i = 0; i < 3; i++) spriteBatch.Draw(texture, drawPos, null, color, 0f, origin, scale, SpriteEffects.None, 0);
+			Effects.EffectsManager.SetSpriteBatchVanillaSettings(spriteBatch);
+			return false;
+		}
+	}
 }
