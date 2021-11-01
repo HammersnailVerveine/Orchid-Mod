5771b385b488e47e1f773eb8c4e25ab23c6390a7
S-Pladison <spladison@gmail.com>
2021-11-02 01:50:52 +0300

    ...

---
+++ Common/Hooks/HookLoader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace OrchidMod.Common.Hooks
+{
+	public static partial class HookLoader
+	{
+		public static void Load(Mod mod)
+		{
+			On.Terraria.Main.DrawProjectiles += On_Terraria_Main_DrawProjectiles;
+			On.Terraria.Main.GUIChatDrawInner += On_Terraria_Main_GUIChatDrawInner;
+			On.Terraria.Player.ScrollHotbar += On_Terraria_Player_ScrollHotbar;
+			On.Terraria.Projectile.NewProjectile_float_float_float_float_int_int_float_int_float_float += On_Terraria_Projectile_NewProjectile;
+		}
+
+		public static void Unload()
+		{
+			On.Terraria.Main.DrawProjectiles -= On_Terraria_Main_DrawProjectiles;
+			On.Terraria.Main.GUIChatDrawInner -= On_Terraria_Main_GUIChatDrawInner;
+			On.Terraria.Player.ScrollHotbar -= On_Terraria_Player_ScrollHotbar;
+			On.Terraria.Projectile.NewProjectile_float_float_float_float_int_int_float_int_float_float -= On_Terraria_Projectile_NewProjectile;
+		}
+	}
+}
---
+++ Common/Hooks/On.Terraria.Main.GUIChatDrawInner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+
+namespace OrchidMod.Common.Hooks
+{
+	public static partial class HookLoader
+	{
+		private static void On_Terraria_Main_GUIChatDrawInner(On.Terraria.Main.orig_GUIChatDrawInner orig, Main self)
+		{
+			orig(self);
+
+			var ui = OrchidMod.Instance?.croupierUI;
+			if (ui == null) return;
+
+			if (ui.Visible)
+			{
+				ui.Update();
+				ui.Draw(Main.spriteBatch);
+			}
+		}
+	}
+}
