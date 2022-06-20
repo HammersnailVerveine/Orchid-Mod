27a485b1d5ce8f459665d19f9086bbade925c4df
Samuel Mounes <samuelpitch@hotmail.fr>
2022-06-20 03:59:46 +0200

    e

--- Gambler/UI/GamblerUIState.cs
+++ Gambler/UI/GamblerUIState.cs
@@ -1,34 +1,441 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System;
 using Terraria;
+using Terraria.GameContent;
+using Terraria.ModLoader;
 using Terraria.UI;
+//using Terraria.UI.Chat;
+using OrchidMod.Gambler.Weapons.Chips;
+using OrchidMod.Common.UIs;
+using System.Collections.Generic;
 
 namespace OrchidMod.Gambler.UI
 {
-	public class GamblerUIState : UIState
+	public class GamblerUIState : OrchidUIState
 	{
-		public GamblerUIFrame gamblerRessourceFrame = new GamblerUIFrame();
+		public Color backgroundColor = Color.White;
+		public static Texture2D ressourceBar;
+		public static Texture2D ressourceBarFull;
+		public static Texture2D ressourceBarDiceFull;
+		public static Texture2D ressourceBarTop;
+		public static Texture2D ressourceBarDiceTop;
+
+		public static Texture2D chip1;
+		public static Texture2D chip2;
+		public static Texture2D chip3;
+		public static Texture2D chip4;
+		public static Texture2D chip5;
+
+		public static Texture2D dice1;
+		public static Texture2D dice2;
+		public static Texture2D dice3;
+		public static Texture2D dice4;
+		public static Texture2D dice5;
+		public static Texture2D dice6;
+
+		public static Texture2D UIDeck;
+		public static Texture2D UICard;
+		public static Texture2D UIRedraw;
+
+		public static Texture2D UICardNext1;
+		public static Texture2D UICardNext2;
+		public static Texture2D UICardNext3;
+
+		public static Texture2D UIDeckbuilding;
+		public static Texture2D UIDeckbuildingBlock;
+		
+		// Chip Weapons
+		
+		public static Texture2D chipDirection;
+		
+		public static Texture2D chipDetonatorMain;
+		public static Texture2D chipDetonatorBar;
+		public static Texture2D chipDetonatorBarEnd;
+		
+		// Chip Weapons end
+
+		public int cardID = -1;
+		public int cardIDNext1 = -1;
+		public int cardIDNext2 = -1;
+		public int cardIDNext3 = -1;
+
+		public override int InsertionIndex(List<GameInterfaceLayer> layers)
+			=> layers.FindIndex(layer => layer.Name.Equals("Vanilla: Mouse Text"));
 
 		public override void OnInitialize()
 		{
-			gamblerRessourceFrame.Width.Set(94f, 0f);
-			gamblerRessourceFrame.Height.Set(180f, 0f);
-			gamblerRessourceFrame.Left.Set(Main.screenWidth / 2 + 30f, 0f);
-			gamblerRessourceFrame.Top.Set(Main.screenHeight / 2 - 50f, 0f);
-			gamblerRessourceFrame.backgroundColor = Color.White;
+			ressourceBar = ModContent.Request<Texture2D>("OrchidMod/Gambler/UI/Textures/GamblerUIBar").Value;
+			ressourceBarFull = ModContent.Request<Texture2D>("OrchidMod/Gambler/UI/Textures/GamblerUIBarFilled").Value;
+			ressourceBarTop = ModContent.Request<Texture2D>("OrchidMod/Gambler/UI/Textures/GamblerUIBarTop").Value;
+			ressourceBarDiceFull = ModContent.Request<Texture2D>("OrchidMod/Gambler/UI/Textures/GamblerUIBarDiceFilled").Value;
+			ressourceBarDiceTop = ModContent.Request<Texture2D>("OrchidMod/Gambler/UI/Textures/GamblerUIBarDiceTop").Value;
 
-			base.Append(gamblerRessourceFrame);
+			chip1 = ModContent.Request<Texture2D>("OrchidMod/Gambler/UI/Textures/UIChip1").Value;
+			chip2 = ModContent.Request<Texture2D>("OrchidMod/Gambler/UI/Textures/UIChip2").Value;
+			chip3 = ModContent.Request<Texture2D>("OrchidMod/Gambler/UI/Textures/UIChip3").Value;
+			chip4 = ModContent.Request<Texture2D>("OrchidMod/Gambler/UI/Textures/UIChip4").Value;
+			chip5 = ModContent.Request<Texture2D>("OrchidMod/Gambler/UI/Textures/UIChip5").Value;
+
+			dice1 = ModContent.Request<Texture2D>("OrchidMod/Gambler/UI/Textures/UIDice1").Value;
+			dice2 = ModContent.Request<Texture2D>("OrchidMod/Gambler/UI/Textures/UIDice2").Value;
+			dice3 = ModContent.Request<Texture2D>("OrchidMod/Gambler/UI/Textures/UIDice3").Value;
+			dice4 = ModContent.Request<Texture2D>("OrchidMod/Gambler/UI/Textures/UIDice4").Value;
+			dice5 = ModContent.Request<Texture2D>("OrchidMod/Gambler/UI/Textures/UIDice5").Value;
+			dice6 = ModContent.Request<Texture2D>("OrchidMod/Gambler/UI/Textures/UIDice6").Value;
+
+			UIDeck = ModContent.Request<Texture2D>("OrchidMod/Gambler/UI/Textures/UIDeck").Value;
+			UIRedraw = ModContent.Request<Texture2D>("OrchidMod/Gambler/UI/Textures/GamblerUIRedraw").Value;
+			UICard = ModContent.Request<Texture2D>("OrchidMod/Gambler/GamblerReset").Value;
+			UICardNext1 = ModContent.Request<Texture2D>("OrchidMod/Gambler/GamblerReset").Value;
+			UICardNext2 = ModContent.Request<Texture2D>("OrchidMod/Gambler/GamblerReset").Value;
+			UICardNext3 = ModContent.Request<Texture2D>("OrchidMod/Gambler/GamblerReset").Value;
+
+			UIDeckbuilding = ModContent.Request<Texture2D>("OrchidMod/Gambler/UI/Textures/DeckUI").Value;
+			UIDeckbuildingBlock = ModContent.Request<Texture2D>("OrchidMod/Gambler/UI/Textures/DeckUIBlock").Value;
+
+			chipDirection = ModContent.Request<Texture2D>("OrchidMod/Gambler/UI/Textures/ChipDirectionArrow").Value;
+			chipDetonatorMain = ModContent.Request<Texture2D>("OrchidMod/Gambler/UI/Textures/ChipDetonatorMain").Value;
+			chipDetonatorBar = ModContent.Request<Texture2D>("OrchidMod/Gambler/UI/Textures/ChipDetonatorBar").Value;
+			chipDetonatorBarEnd = ModContent.Request<Texture2D>("OrchidMod/Gambler/UI/Textures/ChipDetonatorBarEnd").Value;
+
+			Width.Set(94f, 0f);
+			Height.Set(180f, 0f);
+			Left.Set(Main.screenWidth / 2 + 30f, 0f);
+			Top.Set(Main.screenHeight / 2 - 50f, 0f);
+			backgroundColor = Color.White;
+
+			Recalculate();
 		}
 
 		public override void Draw(SpriteBatch spriteBatch)
 		{
-			Recalculate();
-			base.Draw(spriteBatch);
+			Player player = Main.player[Main.myPlayer];
+			OrchidModPlayer modPlayer = player.GetModPlayer<OrchidModPlayer>();
+			
+			Vector2 vector = new Vector2((float)((int)(Main.LocalPlayer.position.X - Main.screenPosition.X) - Main.GameViewMatrix.Translation.X - (float)(Main.LocalPlayer.bodyFrame.Width / 2) + (float)(Main.LocalPlayer.width / 2)), (float)((int)(Main.LocalPlayer.position.Y - Main.screenPosition.Y) - Main.GameViewMatrix.Translation.Y + (float)Main.LocalPlayer.height - (float)Main.LocalPlayer.bodyFrame.Height + 12f + player.gfxOffY)) + Main.LocalPlayer.bodyPosition + new Vector2((float)(Main.LocalPlayer.bodyFrame.Width / 2));
+			vector *= Main.GameViewMatrix.Zoom;
+			vector /= Main.UIScale;
+
+			this.Left.Set(vector.X + 30f, 0f);
+			this.Top.Set(vector.Y - 50f, 0f);
+
+			CalculatedStyle dimensions = GetDimensions();
+			Point point = new Point((int)dimensions.X, (int)dimensions.Y - 100);
+			int width = (int)Math.Ceiling(dimensions.Width);
+			int height = (int)Math.Ceiling(dimensions.Height);
+
+			if (!player.dead)
+			{
+				if (modPlayer.gamblerUIChipSpinDisplay)
+				{
+					Vector2 position = new Vector2(point.X - 30, point.Y + 150);
+					float angle = MathHelper.ToRadians(modPlayer.gamblerChipSpin);
+					spriteBatch.Draw(chipDirection, position, null, backgroundColor, angle, chipDirection.Size() * 0.5f, 1f, SpriteEffects.None, 0f);
+				}
+				
+				if (player.HeldItem.ModItem is MeteorDetonator) {
+					Rectangle rect = new Rectangle(point.X - 75, point.Y + 50, chipDetonatorMain.Width, chipDetonatorMain.Height);
+					spriteBatch.Draw(chipDetonatorMain, rect, backgroundColor);
+					
+					float index = 720f / 8;
+					
+					if (modPlayer.gamblerChipSpin > index) {
+						spriteBatch.Draw(chipDetonatorBar, new Rectangle(point.X - 61, point.Y + 72, 14, 6), backgroundColor);
+					}
+					
+					if (modPlayer.gamblerChipSpin > index * 2 && modPlayer.gamblerChipSpin < index * 7) {
+						spriteBatch.Draw(chipDetonatorBar, new Rectangle(point.X - 45, point.Y + 72, 14, 6), backgroundColor);
+					}
+					
+					if (modPlayer.gamblerChipSpin > index * 3 && modPlayer.gamblerChipSpin < index * 6) {
+						spriteBatch.Draw(chipDetonatorBar, new Rectangle(point.X - 29, point.Y + 72, 14, 6), backgroundColor);
+					}
+					
+					if (modPlayer.gamblerChipSpin > index * 4 && modPlayer.gamblerChipSpin < index * 5) {
+						spriteBatch.Draw(chipDetonatorBarEnd, new Rectangle(point.X - 13, point.Y + 64, 10, 22), backgroundColor);
+					}
+				}
+				
+				/* // OBSOLETE GAMBLER NPC UI CODE
+				if (modPlayer.gamblerUIDeckDisplay)
+				{
+					if (player.talkNPC != -1)
+					{
+						spriteBatch.Draw(UIDeckbuilding, new Rectangle(point.X - 100, point.Y - 60, 140, 138), backgroundColor);
+						int offSetX = 0;
+						int offSetY = 0;
+						string msg = "";
+						string msg2 = "";
+						string msg3 = "";
+						int[] nbCards = new int[20];
+						int maxReq = 0;
+						int playerNbCards = OrchidModGamblerHelper.getNbGamblerCards(player, modPlayer);
+						for (int i = 0; i < 20; i++)
+						{
+							nbCards[i] = 0;
+						}
+						for (int i = 0; i < playerNbCards; i++)
+						{
+							Item currentItem = modPlayer.gamblerCardsItem[i];
+							Texture2D currentTexture = TextureAssets.Item[currentItem.type].Value;
+							Rectangle currentRectangle = new Rectangle(point.X - 92 + offSetX, point.Y - 52 + offSetY, 20, 26);
+							spriteBatch.Draw(currentTexture, currentRectangle, backgroundColor);
+							offSetX += 26;
+							if ((i - 4) % 5 == 0)
+							{
+								offSetX = 0;
+								offSetY += 32;
+							}
+							if (currentItem.type != 0)
+							{
+								OrchidModGlobalItem orchidItem = currentItem.GetGlobalItem<OrchidModGlobalItem>();
+								int cardReq = orchidItem.gamblerCardRequirement;
+								nbCards[cardReq]++;
+								maxReq = cardReq > maxReq ? cardReq : maxReq;
+							}
+						}
+
+						offSetX = 0;
+						offSetY = 0;
+
+						for (int i = 0; i < playerNbCards; i++)
+						{
+							Item currentItem = modPlayer.gamblerCardsItem[i];
+							Rectangle currentRectangle = new Rectangle(point.X - 92 + offSetX, point.Y - 52 + offSetY, 20, 26);
+							offSetX += 26;
+							if ((i - 4) % 5 == 0)
+							{
+								offSetX = 0;
+								offSetY += 32;
+							}
+							if (currentItem.type != 0)
+							{
+								OrchidModGlobalItem orchidItem = currentItem.GetGlobalItem<OrchidModGlobalItem>();
+								int cardReq = orchidItem.gamblerCardRequirement;
+								bool canRemove = (playerNbCards > maxReq + 1) || (cardReq == maxReq);
+								if (currentRectangle.Contains(new Point((int)Main.MouseScreen.X, (int)Main.MouseScreen.Y)))
+								{
+									if (((Main.mouseLeft && Main.mouseLeftRelease) || (Main.mouseRight && Main.mouseRightRelease)) && canRemove)
+									{
+										player.QuickSpawnItem(currentItem.type, 1);
+										OrchidModGamblerHelper.removeGamblerCard(currentItem, player, modPlayer);
+										if (OrchidModGamblerHelper.getNbGamblerCards(player, modPlayer) > 0)
+										{
+											OrchidModGamblerHelper.clearGamblerCardCurrent(player, modPlayer);
+											OrchidModGamblerHelper.clearGamblerCardsNext(player, modPlayer);
+											modPlayer.gamblerShuffleCooldown = 0;
+											modPlayer.gamblerRedraws = 0;
+											OrchidModGamblerHelper.drawGamblerCard(player, modPlayer);
+										}
+										else
+										{
+											OrchidModGamblerHelper.onRespawnGambler(player, modPlayer);
+										}
+									}
+									msg = currentItem.Name;
+									msg2 = canRemove ? "Can be removed" : "Cannot be removed";
+									msg3 = canRemove ? "" : "Your highest cost card requires at least " + maxReq + " cards";
+								}
+								if (!canRemove)
+								{
+									Rectangle blockRectangle = new Rectangle(currentRectangle.X - 2, currentRectangle.Y - 2, currentRectangle.Width + 4, currentRectangle.Height + 4);
+									spriteBatch.Draw(UIDeckbuildingBlock, blockRectangle, backgroundColor);
+								}
+							}
+						}
+						if (msg != "")
+						{
+							ChatManager.DrawColorCodedStringWithShadow(Main.spriteBatch, FontAssets.MouseText.Value, msg, Main.MouseScreen + new Vector2(15f, 15f), new Color(Main.mouseTextColor, Main.mouseTextColor, Main.mouseTextColor, Main.mouseTextColor), 0f, Vector2.Zero, Vector2.One, -1f, 2f);
+						}
+						if (msg2 != "")
+						{
+							ChatManager.DrawColorCodedStringWithShadow(Main.spriteBatch, FontAssets.MouseText.Value, msg2, Main.MouseScreen + new Vector2(15f, 40f), new Color(Main.mouseTextColor, Main.mouseTextColor, Main.mouseTextColor, Main.mouseTextColor), 0f, Vector2.Zero, Vector2.One, -1f, 2f);
+						}
+						if (msg3 != "")
+						{
+							ChatManager.DrawColorCodedStringWithShadow(Main.spriteBatch, FontAssets.MouseText.Value, msg2, Main.MouseScreen + new Vector2(15f, 40f), new Color(Main.mouseTextColor, Main.mouseTextColor, Main.mouseTextColor, Main.mouseTextColor), 0f, Vector2.Zero, Vector2.One, -1f, 2f);
+						}
+					}
+					else
+					{
+						modPlayer.gamblerUIDeckDisplay = false;
+					}
+				}
+				*/
+
+				if ((modPlayer.gamblerUIDisplayTimer > 0 || modPlayer.gamblerChips > 0) && modPlayer.gamblerUIFightDisplay)
+				{
+
+					if (modPlayer.gamblerCardCurrent.type != this.cardID)
+					{
+						UICard = this.getUiCardTexture(modPlayer, 0);
+						this.cardID = modPlayer.gamblerCardCurrent.type;
+					}
+
+					if (modPlayer.gamblerCardNext[0].type != this.cardIDNext1)
+					{
+						UICardNext1 = this.getUiCardTexture(modPlayer, 1);
+						this.cardIDNext1 = modPlayer.gamblerCardNext[0].type;
+					}
+
+					if (modPlayer.gamblerCardNext[1].type != this.cardIDNext2)
+					{
+						UICardNext2 = this.getUiCardTexture(modPlayer, 2);
+						this.cardIDNext2 = modPlayer.gamblerCardNext[1].type;
+					}
+
+					if (modPlayer.gamblerCardNext[2].type != this.cardIDNext3)
+					{
+						UICardNext3 = this.getUiCardTexture(modPlayer, 3);
+						this.cardIDNext3 = modPlayer.gamblerCardNext[2].type;
+					}
+
+					int drawHeight = 70;
+					int drawSide = 0;
+					int leftOffset = 0;
+					int leftOffset2 = 0;
+					Texture2D chip = chip1;
+
+					spriteBatch.Draw(UIDeck, new Rectangle(point.X + 8, point.Y + 160, 26, 32), backgroundColor);
+					spriteBatch.Draw(UICard, new Rectangle(point.X + 8, point.Y + 160, 20, 26), backgroundColor);
+
+					if (modPlayer.gamblerSeeCards > 2) spriteBatch.Draw(UICardNext3, new Rectangle(point.X + 40, point.Y + 194, 20, 26), backgroundColor);
+					if (modPlayer.gamblerSeeCards > 1) spriteBatch.Draw(UICardNext2, new Rectangle(point.X + 24, point.Y + 194, 20, 26), backgroundColor);
+					if (modPlayer.gamblerSeeCards > 0) spriteBatch.Draw(UICardNext1, new Rectangle(point.X + 8, point.Y + 194, 20, 26), backgroundColor);
+
+					for (int i = 0; i < modPlayer.gamblerRedraws; i++)
+					{
+						spriteBatch.Draw(UIRedraw, new Rectangle(point.X + 20 - drawSide, point.Y + 94, 14, 20), backgroundColor);
+						drawSide += 4;
+					}
+
+					int unit = (int)(modPlayer.gamblerShuffleCooldownMax / 34) + 1;
+					int val = 0;
+					spriteBatch.Draw(ressourceBar, new Rectangle(point.X + 36, point.Y + 116, 12, 76), backgroundColor);
+					while (val < modPlayer.gamblerShuffleCooldown)
+					{
+						spriteBatch.Draw(ressourceBarFull, new Rectangle(point.X + 40, point.Y + 116 + drawHeight, 4, 2), backgroundColor);
+						drawHeight -= 2;
+						val += unit;
+					}
+
+					if (val > 0)
+					{
+						drawHeight += 2;
+						spriteBatch.Draw(ressourceBarTop, new Rectangle(point.X + 40, point.Y + 116 + drawHeight, 4, 2), backgroundColor);
+					}
+
+					if (modPlayer.gamblerDiceValue > 0)
+					{
+						Texture2D dice = dice1;
+						switch (modPlayer.gamblerDiceValue)
+						{
+							case 1:
+								dice = dice1;
+								break;
+							case 2:
+								dice = dice2;
+								break;
+							case 3:
+								dice = dice3;
+								break;
+							case 4:
+								dice = dice4;
+								break;
+							case 5:
+								dice = dice5;
+								break;
+							case 6:
+								dice = dice6;
+								break;
+							default:
+								break;
+						}
+						spriteBatch.Draw(dice, new Rectangle(point.X + 36, point.Y + 90, 24, 24), backgroundColor);
+
+						drawHeight = 70;
+						val = 0;
+
+						spriteBatch.Draw(ressourceBar, new Rectangle(point.X + 50, point.Y + 116, 12, 76), backgroundColor);
+						while (val < modPlayer.gamblerDiceDuration)
+						{
+							spriteBatch.Draw(ressourceBarDiceFull, new Rectangle(point.X + 54, point.Y + 116 + drawHeight, 4, 2), backgroundColor);
+							drawHeight -= 2;
+							val += 60;
+						}
+
+						if (val > 0)
+						{
+							spriteBatch.Draw(ressourceBarDiceTop, new Rectangle(point.X + 54, point.Y + 116 + drawHeight, 4, 2), backgroundColor);
+						}
+					}
+
+					drawHeight = 36;
+					for (int i = 1; i < modPlayer.gamblerChips + 1; i++)
+					{
+						spriteBatch.Draw(chip, new Rectangle(point.X + 18 - leftOffset - leftOffset2, point.Y + 116 + drawHeight, 14, 6), backgroundColor);
+
+						drawHeight -= 4;
+						if (i % 10 == 0)
+						{
+							drawHeight = 36;
+							leftOffset2 = leftOffset2 == 4 ? 0 : 4;
+							switch (i)
+							{
+								case 10:
+									chip = chip2;
+									break;
+								case 20:
+									chip = chip3;
+									break;
+								case 30:
+									chip = chip4;
+									break;
+								case 40:
+									chip = chip5;
+									break;
+								default:
+									break;
+							}
+						}
+						leftOffset = leftOffset == 2 ? 0 : 2;
+					}
+				}
+			}
 		}
 
-		public override void Update(GameTime gameTime)
+		public Texture2D getUiCardTexture(OrchidModPlayer modPlayer, int cardNb)
 		{
-			base.Update(gameTime);
+			Texture2D cardTexture;
+			Item card = new Item();
+			if (modPlayer.gamblerCardCurrent.type != 0)
+			{
+				switch (cardNb)
+				{
+					case 0:
+						card = modPlayer.gamblerCardCurrent;
+						break;
+					case 1:
+						card = modPlayer.gamblerCardNext[0];
+						break;
+					case 2:
+						card = modPlayer.gamblerCardNext[1];
+						break;
+					case 3:
+						card = modPlayer.gamblerCardNext[2];
+						break;
+					default:
+						cardTexture = UIRedraw;
+						return cardTexture;
+				}
+				cardTexture = TextureAssets.Item[card.type].Value;
+			}
+			else
+			{
+				cardTexture = UIRedraw;
+			}
+			return cardTexture;
 		}
 	}
 }
--- Guardian/UI/GuardianUIState.cs
+++ Guardian/UI/GuardianUIState.cs
@@ -1,34 +1,95 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using OrchidMod.Common.UIs;
+using System;
+using System.Collections.Generic;
 using Terraria;
+using Terraria.ModLoader;
 using Terraria.UI;
 
 namespace OrchidMod.Guardian.UI
 {
-	public class GuardianUIState : UIState
+	public class GuardianUIState : OrchidUIState
 	{
-		public GuardianUIFrame guardianRessourceFrame = new GuardianUIFrame();
+		public Color backgroundColor = Color.White;
+		public static Texture2D textureBlockOn;
+		public static Texture2D textureBlockOff;
+		public static Texture2D textureSlamOn;
+		public static Texture2D textureSlamOff;
 
+		public override int InsertionIndex(List<GameInterfaceLayer> layers)
+			=> layers.FindIndex(layer => layer.Name.Equals("Vanilla: Mouse Text"));
 		public override void OnInitialize()
 		{
-			guardianRessourceFrame.Width.Set(10f, 0f);
-			guardianRessourceFrame.Height.Set(10f, 0f);
-			guardianRessourceFrame.Left.Set(Main.screenWidth / 2, 0f);
-			guardianRessourceFrame.Top.Set(Main.screenHeight / 2, 0f);
-			guardianRessourceFrame.backgroundColor = Color.White;
+			textureBlockOn = ModContent.Request<Texture2D>("OrchidMod/Guardian/UI/Textures/BlockBarOn").Value;
+			textureBlockOff = ModContent.Request<Texture2D>("OrchidMod/Guardian/UI/Textures/BlockBarOff").Value;
+			textureSlamOn = ModContent.Request<Texture2D>("OrchidMod/Guardian/UI/Textures/SlamBarOn").Value;
+			textureSlamOff = ModContent.Request<Texture2D>("OrchidMod/Guardian/UI/Textures/SlamBarOff").Value;
 
-			base.Append(guardianRessourceFrame);
-		}
+			Width.Set(10f, 0f);
+			Height.Set(10f, 0f);
+			Left.Set(Main.screenWidth / 2, 0f);
+			Top.Set(Main.screenHeight / 2, 0f);
+			backgroundColor = Color.White;
 
-		public override void Draw(SpriteBatch spriteBatch)
-		{
 			Recalculate();
-			base.Draw(spriteBatch);
 		}
 
-		public override void Update(GameTime gameTime)
+		protected override void DrawSelf(SpriteBatch spriteBatch)
 		{
-			base.Update(gameTime);
+			Player player = Main.LocalPlayer;
+
+			Vector2 vector = (player.position + new Vector2(player.width * 0.5f, player.gravDir > 0 ? player.height - 10 + player.gfxOffY : 10 + player.gfxOffY)).Floor();
+			vector = Vector2.Transform(vector - Main.screenPosition, Main.GameViewMatrix.EffectMatrix * Main.GameViewMatrix.ZoomMatrix) / Main.UIScale;
+
+			this.Left.Set(vector.X, 0f);
+			this.Top.Set(vector.Y, 0f);
+
+			CalculatedStyle dimensions = GetDimensions();
+			Point point = new Point((int)dimensions.X, (int)dimensions.Y + 20);
+			int width = (int)Math.Ceiling(dimensions.Width);
+			int height = (int)Math.Ceiling(dimensions.Height);
+			OrchidModPlayer modPlayer = player.GetModPlayer<OrchidModPlayer>();
+
+			if (!player.dead)
+			{
+				if (modPlayer.guardianDisplayUI > 0)
+				{
+					// Texture = 16*16
+					int offSet = (int)(modPlayer.guardianBlockMax / 2 * 18) + 8;
+					for (int i = 0 ; i < modPlayer.guardianBlockMax ; i ++) {
+						Texture2D texture = modPlayer.guardianBlock > i ? textureBlockOn : textureBlockOff;
+						spriteBatch.Draw(texture, new Rectangle(point.X - offSet + 18 * i, point.Y, 16, 16), backgroundColor);
+					}
+					
+					for (int i = 0 ; i < modPlayer.guardianSlamMax ; i ++) {
+						Texture2D texture = modPlayer.guardianSlam > i ? textureSlamOn : textureSlamOff;
+						spriteBatch.Draw(texture, new Rectangle(point.X - offSet + 18 * i, point.Y + 18, 16, 16), backgroundColor);
+					}
+					
+
+					
+					int projectileType = ModContent.ProjectileType<GuardianShieldAnchor>();
+					for (int i = 0 ; i < Main.projectile.Length ; i ++)
+					{
+						Projectile proj = Main.projectile[i];
+						if (proj.active && proj.owner == player.whoAmI && proj.type == projectileType && proj.ai[0] > 0f && proj.localAI[0] > 0f)
+						{
+							int val = 11;
+							float block = proj.ai[0];
+							while (block < proj.localAI[0]) 
+							{
+								block += proj.localAI[0] / 10f;
+								val --;
+							}
+							val = val > 10 ? 10 : val;
+							Texture2D texture = ModContent.Request<Texture2D>("OrchidMod/Guardian/UI/Textures/BlockDuration" + val).Value;
+							spriteBatch.Draw(texture, new Rectangle(point.X - 10, point.Y - 90, 20, 22), backgroundColor);
+							return;
+						}
+					}
+				}
+			}
 		}
 	}
 }
