fb9d8e13ca9303babe8ce73d35cfb47a577d8bd2
S-Pladison <spladison@gmail.com>
2022-06-23 18:57:51 +0300

    Prefixes (I)

---
+++ Content/Prefixes/OrchidPrefixes.cs
@@ -0,0 +1,44 @@
+﻿namespace OrchidMod.Content.Prefixes
+{
+	// Shaman (I)
+
+	public class VoodooPrefix : ShamanPrefix { public VoodooPrefix() : base("Voodoo", 1.00f, 1.00f, 1.00f, 1.00f, 1.05f) { } }
+	public class SuperiorPrefix : ShamanPrefix { public SuperiorPrefix() : base("Superior", 1.10f, 1.10f, 1.00f, 1.00f, 1.00f) { } }
+	public class ForcefulPrefix : ShamanPrefix { public ForcefulPrefix() : base("Forceful", 1.00f, 1.15f, 1.00f, 1.00f, 1.00f) { } }
+	public class BrokenPrefix : ShamanPrefix { public BrokenPrefix() : base("Broken", 0.70f, 0.80f, 1.00f, 1.00f, 1.00f) { } }
+	public class DamagedPrefix : ShamanPrefix { public DamagedPrefix() : base("Damaged", 0.85f, 1.00f, 1.00f, 1.00f, 1.00f) { } }
+	public class ShoddyPrefix : ShamanPrefix { public ShoddyPrefix() : base("Shoddy", 0.90f, 0.85f, 1.00f, 1.00f, 1.00f) { } }
+	public class HurtfulPrefix : ShamanPrefix { public HurtfulPrefix() : base("Hurtful", 1.10f, 1.00f, 1.00f, 1.00f, 1.00f) { } }
+	public class StrongPrefix : ShamanPrefix { public StrongPrefix() : base("Strong", 1.00f, 1.15f, 1.00f, 1.00f, 1.00f) { } }
+	public class UnpleasantPrefix : ShamanPrefix { public UnpleasantPrefix() : base("Unpleasant", 1.05f, 1.15f, 1.00f, 1.00f, 1.00f) { } }
+	public class WeakPrefix : ShamanPrefix { public WeakPrefix() : base("Weak", 1.00f, 0.80f, 1.00f, 1.00f, 1.00f) { } }
+	public class RuthlessPrefix : ShamanPrefix { public RuthlessPrefix() : base("Ruthless", 1.18f, 0.90f, 1.00f, 1.00f, 1.00f) { } }
+	public class OccultPrefix : ShamanPrefix { public OccultPrefix() : base("Occult", 1.15f, 1.15f, 1.00f, 1.00f, 1.10f) { } }
+	public class DiabolicPrefix : ShamanPrefix { public DiabolicPrefix() : base("Diabolic", 1.15f, 1.00f, 1.00f, 1.00f, 1.10f) { } }
+	public class SpiritedPrefix : ShamanPrefix { public SpiritedPrefix() : base("Spirited", 1.00f, 1.00f, 1.00f, 1.00f, 1.10f) { } }
+
+	// Shaman (II)
+
+	public class QuickPrefix : ShamanPrefix { public QuickPrefix() : base("Quick", 1.00f, 1.00f, 0.90f, 1.00f, 1.00f) { } }
+	public class DeadlyPrefix : ShamanPrefix { public DeadlyPrefix() : base("Deadly", 1.10f, 1.00f, 0.90f, 1.00f, 1.00f) { } }
+	public class MagneticPrefix : ShamanPrefix { public MagneticPrefix() : base("Magnetic", 1.00f, 1.00f, 0.90f, 1.00f, 1.05f) { } }
+	public class NimblePrefix : ShamanPrefix { public NimblePrefix() : base("Nimble", 1.00f, 1.00f, 0.95f, 1.00f, 1.00f) { } }
+	public class RunicPrefix : ShamanPrefix { public RunicPrefix() : base("Runic", 1.07f, 1.00f, 0.94f, 1.00f, 1.05f) { } }
+	public class SlowPrefix : ShamanPrefix { public SlowPrefix() : base("Slow", 1.00f, 1.00f, 1.15f, 1.00f, 1.00f) { } }
+	public class SluggishPrefix : ShamanPrefix { public SluggishPrefix() : base("Sluggish", 1.00f, 1.00f, 1.20f, 1.00f, 1.00f) { } }
+	public class LazyPrefix : ShamanPrefix { public LazyPrefix() : base("Lazy", 1.00f, 1.00f, 1.18f, 1.00f, 1.00f) { } }
+	public class AnnoyingPrefix : ShamanPrefix { public AnnoyingPrefix() : base("Annoying", 0.80f, 1.00f, 1.15f, 1.00f, 1.00f) { } }
+	public class ConjuringPrefix : ShamanPrefix { public ConjuringPrefix() : base("Conjuring", 1.05f, 0.90f, 0.90f, 1.00f, 1.05f) { } }
+
+	// Shaman (III)
+
+	public class StudiousPrefix : ShamanPrefix { public StudiousPrefix() : base("Studious", 1.10f, 1.00f, 1.00f, 1.15f, 1.00f) { } }
+	public class UniquePrefix : ShamanPrefix { public UniquePrefix() : base("Unique", 1.15f, 1.05f, 1.00f, 1.20f, 1.00f) { } }
+	public class BalancedPrefix : ShamanPrefix { public BalancedPrefix() : base("Balanced", 1.00f, 1.10f, 0.90f, 1.00f, 1.00f) { } }
+	public class HopefulPrefix : ShamanPrefix { public HopefulPrefix() : base("Hopeful", 1.00f, 1.00f, 1.00f, 1.15f, 1.00f) { } }
+	public class EnragedPrefix : ShamanPrefix { public EnragedPrefix() : base("Enraged", 1.15f, 1.15f, 1.00f, 1.00f, 1.00f) { } }
+	public class EffervescentPrefix : ShamanPrefix { public EffervescentPrefix() : base("Effervescent", 1.10f, 1.10f, 1.10f, 1.10f, 1.00f) { } }
+	public class EtherealPrefix : ShamanPrefix { public EtherealPrefix() : base("Ethereal", 1.15f, 1.15f, 0.90f, 1.10f, 1.10f) { } }
+	public class FocusedPrefix : ShamanPrefix { public FocusedPrefix() : base("Focused", 1.10f, 1.00f, 1.00f, 1.15f, 1.00f) { } }
+	public class ComplexPrefix : ShamanPrefix { public ComplexPrefix() : base("Complex", 0.90f, 1.00f, 0.90f, 1.10f, 1.00f) { } }
+}
---
+++ Content/Prefixes/ShamanPrefix.cs
@@ -0,0 +1,170 @@
+using System.Collections.Generic;
+using System.IO;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Utilities;
+
+namespace OrchidMod.Content.Prefixes
+{
+	public abstract class ShamanPrefix : ModPrefix
+	{
+		private static readonly List<ShamanPrefix> prefixes = new();
+		public static IReadOnlyList<ShamanPrefix> GetPrefixes => prefixes;
+
+		// ...
+
+		private readonly string displayName;
+		private readonly float damage;
+		private readonly float crit;
+		private readonly float useTime;
+		private readonly float velocity;
+		private readonly float knockback;
+
+		public bool UseTimeHasDefaultValue
+			=> useTime == 1f;
+
+		public bool VelocityHasDefaultValue
+			=> velocity == 1f;
+
+		public override void Unload()
+			=> prefixes.Clear();
+
+		public override float RollChance(Item item)
+			=> 500f;
+
+		public override bool CanRoll(Item item)
+			=> true;
+
+		public override PrefixCategory Category
+			=> PrefixCategory.Custom;
+
+		public ShamanPrefix(string displayName, float damage, float knockback, float useTime, float crit, float velocity)
+		{
+			this.displayName = displayName;
+			this.damage = damage;
+			this.knockback = knockback;
+			this.useTime = useTime;
+			this.crit = crit;
+			this.velocity = velocity;
+		}
+
+		public override void Load()
+			=> prefixes.Add(this);
+
+		public override void SetStaticDefaults()
+			=> DisplayName.SetDefault(displayName);
+
+		public override void Apply(Item item)
+			=> item.GetGlobalItem<ShamanPrefixItem>().SetPrefixVariables(damage, knockback, useTime, crit, velocity);
+
+		public override void ModifyValue(ref float valueMult)
+		{
+			float multiplier = 1f * (damage * 0.96f) * (knockback * 0.96f) * (crit * 0.96f) * ((2f - useTime) * 0.96f) * (velocity * 0.96f);
+			valueMult *= multiplier;
+		}
+
+		public override void SetStats(ref float damageMult, ref float knockbackMult, ref float useTimeMult,
+		ref float scaleMult, ref float shootSpeedMult, ref float manaMult, ref int critBonus)
+		{
+			damageMult = damage;
+			knockbackMult = knockback;
+			useTimeMult = useTime;
+			critBonus = (int)(crit * 100 - 100);
+			shootSpeedMult = velocity;
+		}
+	}
+
+	public class ShamanPrefixItem : GlobalItem
+	{
+		private float damage;
+		private float crit;
+		private float useTime;
+		private float velocity;
+		private float knockback;
+
+		// ...
+
+		public void SetPrefixVariables(float damage, float knockback, float useTime, float crit, float velocity)
+		{
+			this.damage = damage;
+			this.knockback = knockback;
+			this.useTime = useTime;
+			this.crit = crit;
+			this.velocity = velocity;
+		}
+
+		// ...
+
+		public ShamanPrefixItem()
+		{
+			damage = 0;
+			crit = 0;
+			useTime = 0;
+			velocity = 0;
+			knockback = 0;
+		}
+
+		public override bool InstancePerEntity
+			=> true;
+
+		public override GlobalItem Clone(Item item, Item itemClone)
+		{
+			ShamanPrefixItem myClone = (ShamanPrefixItem)base.Clone(item, itemClone);
+			myClone.damage = damage;
+			myClone.crit = crit;
+			myClone.useTime = useTime;
+			myClone.velocity = velocity;
+			myClone.knockback = knockback;
+			return myClone;
+		}
+
+		public override bool PreReforge(Item item)
+		{
+			damage = 0;
+			crit = 0;
+			useTime = 0;
+			velocity = 0;
+			knockback = 0;
+
+			return base.PreReforge(item);
+		}
+
+		public override int ChoosePrefix(Item item, UnifiedRandom rand)
+		{
+			if (item.damage <= 0 || item.accessory || item.type == ItemID.None) return -1;
+
+			var globalItem = item.GetGlobalItem<OrchidModGlobalItem>();
+			if (!globalItem.shamanWeapon) return -1;
+
+			var prefixes = ShamanPrefix.GetPrefixes;
+			while (true)
+			{
+				var prefix = prefixes[Main.rand.Next(prefixes.Count)];
+
+				if (globalItem.shamanWeaponNoUsetimeReforge && !prefix.UseTimeHasDefaultValue) continue;
+				if (globalItem.shamanWeaponNoVelocityReforge && !prefix.VelocityHasDefaultValue) continue;
+
+				return prefix.Type;
+			}
+		}
+
+		public override void NetSend(Item item, BinaryWriter writer)
+		{
+			writer.Write(damage);
+			writer.Write(knockback);
+			writer.Write(useTime);
+			writer.Write(crit);
+			writer.Write(velocity);
+		}
+
+		public override void NetReceive(Item item, BinaryReader reader)
+		{
+			damage = reader.ReadSingle();
+			knockback = reader.ReadSingle();
+			useTime = reader.ReadSingle();
+			crit = reader.ReadSingle();
+			velocity = reader.ReadSingle();
+		}
+	}
+}
