b20a06edd6bf7ff367dd7abdc70b7b1ca0ec3cb9
S-Pladison <spladison@gmail.com>
2022-07-19 03:24:29 +0300

    Removed 'Hooks' folder

--- Common/IOnMouseHover.cs
+++ Common/IOnMouseHover.cs
@@ -1,9 +1,29 @@
-﻿using Terraria.UI;
+﻿using Terraria.ModLoader;
 
 namespace OrchidMod.Common
 {
 	public interface IOnMouseHover
 	{
 		void OnMouseHover(int context);
+
+		private class HookLoader : ILoadable
+		{
+			void ILoadable.Load(Mod mod)
+			{
+				On.Terraria.UI.ItemSlot.OverrideHover_ItemArray_int_int += (orig, inv, context, slot) =>
+				{
+					orig(inv, context, slot);
+
+					var item = inv[slot];
+
+					if (item.ModItem is IOnMouseHover obj)
+					{
+						obj.OnMouseHover(context);
+					}
+				};
+			}
+
+			void ILoadable.Unload() { }
+		}
 	}
 }
