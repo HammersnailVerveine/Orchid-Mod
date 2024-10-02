b046123570d227dc6545755c441eb2d0d84d710e
Samuel Mounes <samuelpitch@hotmail.fr>
2024-10-02 08:08:51 +0200

    Added Cobalt and Palladium Gauntlets

---
+++ Content/Guardian/Weapons/Gauntlets/CobaltGauntlet.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+
+namespace OrchidMod.Content.Guardian.Weapons.Gauntlets
+{
+	public class CobaltGauntlet : OrchidModGuardianGauntlet
+	{
+		public override void SafeSetDefaults()
+		{
+			Item.width = 28;
+			Item.height = 32;
+			Item.knockBack = 5f;
+			Item.damage = 187;
+			Item.value = Item.sellPrice(0, 0, 90, 0);
+			Item.rare = ItemRarityID.LightRed;
+			Item.useTime = 20;
+			strikeVelocity = 22.5f;
+			parryDuration = 75;
+		}
+
+		public override Color GetColor(bool offHand)
+		{
+			return new Color(113, 217, 251);
+		}
+
+		public override void AddRecipes()
+		{
+			var recipe = CreateRecipe();
+			recipe.AddTile(TileID.Anvils);
+			recipe.AddIngredient(ItemID.CobaltBar, 10);
+			recipe.Register();
+		}
+	}
+}
---
+++ Content/Guardian/Weapons/Gauntlets/PalladiumGauntlet.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+
+namespace OrchidMod.Content.Guardian.Weapons.Gauntlets
+{
+	public class PalladiumGauntlet : OrchidModGuardianGauntlet
+	{
+		public override void SafeSetDefaults()
+		{
+			Item.width = 30;
+			Item.height = 32;
+			Item.knockBack = 5f;
+			Item.damage = 189;
+			Item.value = Item.sellPrice(0, 1, 20, 0);
+			Item.rare = ItemRarityID.LightRed;
+			Item.useTime = 20;
+			strikeVelocity = 22.5f;
+			parryDuration = 85;
+		}
+
+		public override Color GetColor(bool offHand)
+		{
+			return new Color(247, 183, 51);
+		}
+
+		public override void AddRecipes()
+		{
+			var recipe = CreateRecipe();
+			recipe.AddTile(TileID.Anvils);
+			recipe.AddIngredient(ItemID.PalladiumBar, 10);
+			recipe.Register();
+		}
+	}
+}
