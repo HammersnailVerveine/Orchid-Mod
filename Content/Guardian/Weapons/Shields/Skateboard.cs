f5246126a2f488eefbeef2e1f38cabe1b07098af
Samuel Mounes <samuelpitch@hotmail.fr>
2024-10-05 21:54:46 +0200

    Fixed skateboard

--- Content/Guardian/Weapons/Shields/Skateboard.cs
+++ Content/Guardian/Weapons/Shields/Skateboard.cs
@@ -10,6 +10,7 @@ namespace OrchidMod.Content.Guardian.Weapons.Shields
 	public class Skateboard : OrchidModGuardianShield
 	{
 		public static readonly SoundStyle SoundTrick = new(OrchidAssets.SoundsPath + "SkateTrick");
+		public float playerVelocity;
 
 		public override void SafeSetDefaults()
 		{
@@ -28,7 +29,7 @@ namespace OrchidMod.Content.Guardian.Weapons.Shields
 
 		public override void BlockStart(Player player, Projectile shield)
 		{
-			shield.ai[2] = 0;
+			playerVelocity = 0;
 		}
 
 		public override void SlamHitFirst(Player player, Projectile shield, NPC npc)
@@ -53,36 +54,35 @@ namespace OrchidMod.Content.Guardian.Weapons.Shields
 			if (projectile.ai[0] > 0f && projectile.ModProjectile is GuardianShieldAnchor anchor) // is blocking
 			{
 				Player owner = Main.player[projectile.owner];
-				if (projectile.ai[2] != 0f && (owner.velocity.X == 0f || owner.grapCount > 0 && owner.mount.Type != MountID.None)) // Player hit a tile, stop skating
+				if (playerVelocity != 0f && (owner.velocity.X == 0f || owner.grapCount > 0 && owner.mount.Type != MountID.None)) // Player hit a tile, stop skating
 				{
 					projectile.ai[0] = 1f;
-					if (Main.rand.NextBool(4)) SoundEngine.PlaySound(SoundID.Item55, projectile.Center);
 				}
 
 				// playing is aiming down has no hook or mount, and is falling. Place the shield down and allow skating
-				if (anchor.aimedLocation.Y > owner.Center.Y && (Math.Abs(anchor.aimedLocation.X - owner.Center.X) < 32f) && owner.grapCount == 0 && owner.mount.Type == MountID.None && (owner.velocity.Y > 1f || projectile.ai[2] != 0f))
+				if (anchor.aimedLocation.Y > owner.Center.Y && (Math.Abs(anchor.aimedLocation.X - owner.Center.X) < 32f) && owner.grapCount == 0 && owner.mount.Type == MountID.None && (owner.velocity.Y > 1f || playerVelocity != 0f))
 				{
 					anchor.aimedLocation = owner.Center.Floor() - new Vector2(projectile.width / 2f, projectile.height / 2f) + Vector2.UnitY * distance;
-					anchor.Projectile.ai[2] = -MathHelper.PiOver2; // networkedrotation
+					projectile.ai[2] = -MathHelper.PiOver2; // networkedrotation
 					projectile.rotation = -MathHelper.PiOver2;
 
 					// Collision with the ground, do skating stuff
-					Vector2 collision = Collision.TileCollision(owner.position + new Vector2((owner.width - Item.width) * 0.5f, owner.height), Vector2.UnitY * 8f, Item.width, 14, false, false, (int)owner.gravDir);
-					if (collision != Vector2.UnitY * 8f)
+					Vector2 collision = Collision.TileCollision(owner.position + new Vector2((owner.width - Item.width) * 0.5f, owner.height), Vector2.UnitY * 12f, Item.width, 14, false, false, (int)owner.gravDir);
+					if (collision != Vector2.UnitY * 12f)
 					{
 						owner.fallStart = (int)(owner.position.Y / 16f);
 						owner.fallStart2 = (int)(owner.position.Y / 16f);
 						owner.position.Y += (collision.Y - 1.7f);
-						owner.velocity.X = projectile.ai[2];
+						owner.velocity.X = playerVelocity;
 						owner.velocity.Y = 0.1f;
 
 						if (Main.rand.NextBool(4)) SoundEngine.PlaySound(SoundID.Item55, projectile.Center);
 					}
 					else
 					{
-						if (projectile.ai[2] == 0) SoundEngine.PlaySound(SoundID.Item53, projectile.Center);
-						projectile.ai[2] = owner.velocity.X;
-						if (Math.Abs(projectile.ai[2]) < 8f) projectile.ai[2] = 8f * Math.Sign(projectile.ai[2]);
+						if (playerVelocity == 0) SoundEngine.PlaySound(SoundID.Item53, projectile.Center);
+						playerVelocity = owner.velocity.X;
+						if (Math.Abs(playerVelocity) < 8f) playerVelocity = 8f * Math.Sign(playerVelocity);
 					}
 				}
 			}
