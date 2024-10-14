b8c03b15ddfbbda942b38d5bfa72e1c2c950f006
Samuel Mounes <samuelpitch@hotmail.fr>
2024-10-14 18:21:46 +0200

    more owl

--- Content/Shapeshifter/Weapons/Sage/SageOwl.cs
+++ Content/Shapeshifter/Weapons/Sage/SageOwl.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using OrchidMod.Common.ModObjects;
 using OrchidMod.Utilities;
+using System;
 using System.Collections.Generic;
 using Terraria;
 using Terraria.Audio;
@@ -16,6 +17,7 @@ namespace OrchidMod.Content.Shapeshifter.Weapons.Sage
 	{
 		bool WasGliding = false;
 		bool WasAscending = false;
+		bool Landed = false;
 
 		public override void SafeSetDefaults()
 		{
@@ -39,6 +41,163 @@ namespace OrchidMod.Content.Shapeshifter.Weapons.Sage
 			projectile.spriteDirection = owner.direction;
 		}
 
+		public override void ShapeshiftAnchorAI(Projectile projectile, ShapeshifterShapeshiftAnchor anchor)
+		{
+			Player owner = Main.player[projectile.owner];
+			Vector2 intendedVelocity = projectile.velocity;
+			anchor.Timespent++;
+
+			if (anchor.Timespent % 6 == 0 && anchor.Timespent > 0)
+			{ // Animation frames
+				anchor.Frame++;
+
+				if (anchor.Frame == 2)
+				{
+					anchor.Timespent = -5;
+				}
+
+				if (anchor.Frame == 1)
+				{
+					anchor.Timespent = -3;
+				}
+
+				if (anchor.Frame == 7)
+				{
+					anchor.Frame = 1;
+				}
+			}
+
+			bool grounded = false;
+			for (int i = 0; i < 10; i++)
+			{ // Checks if the player/projectile is within 2 tiles of the ground
+				if (Collision.TileCollision(projectile.position + Vector2.UnitY * 3.2f * i, Vector2.UnitY * 3.2f, projectile.width, projectile.height, false, false, (int)owner.gravDir) != Vector2.UnitY * 3.2f)
+				{
+					grounded = true;
+					break;
+				}
+			}
+
+			if (grounded)
+			{
+				intendedVelocity.Y += 0.05f;
+				if (anchor.Timespent < 0) anchor.Timespent = 0;
+			}
+			else
+			{
+				intendedVelocity.Y += 0.15f;
+			}
+
+
+			if (owner.controlDown || owner.controlUp || owner.controlJump)
+			{ // Vertical movement
+				if (anchor.Frame < 0) anchor.Frame = 0;
+				if (owner.controlJump || owner.controlUp)
+				{
+					if (owner.controlJump && grounded)
+					{ // Player is grounded and presses up. Propels the player upwards
+
+					}
+					else
+					{ // Slowly glides down
+						if (anchor.Frame == 4) anchor.Frame = 3;
+						intendedVelocity.Y = 0.8f;
+						WasGliding = true;
+					}
+				}
+				else
+				{ // Stops flapping the wings, falling down faster
+					if (anchor.Frame == 4 || anchor.Frame == 3)
+					{
+						anchor.Frame = 2;
+					}
+					intendedVelocity.Y += 0.05f;
+				}
+			}
+			else if (anchor.Frame == 3 || WasGliding)
+			{ // Idle (slowly falling)
+				intendedVelocity.Y = WasGliding ? 0 : -1f;
+				SoundEngine.PlaySound(SoundID.Item32, projectile.Center);
+				WasGliding = false;
+			}
+
+			if (owner.controlLeft || owner.controlRight)
+			{
+				if (Landed)
+				{ // Kickstart if the owl was landed
+					Landed = false;
+					intendedVelocity.Y = -2f;
+				}
+
+				if (grounded)
+				{ // Helps staying a bit more over the ground while moving left and right
+					if (intendedVelocity.Y > -0.1f)
+					{
+						intendedVelocity.Y = -0.1f;
+					}
+				}
+
+				if (owner.controlLeft && !owner.controlRight)
+				{ // Left movement
+					intendedVelocity.X -= 0.25f;
+					if (intendedVelocity.X < -5f) intendedVelocity.X = -5f;
+					projectile.direction = -1;
+					projectile.spriteDirection = -1;
+				}
+				else if (owner.controlRight && !owner.controlLeft)
+				{ // Right movement
+					intendedVelocity.X += 0.25f;
+					if (intendedVelocity.X > 5f) intendedVelocity.X = 5f;
+					projectile.direction = 1;
+					projectile.spriteDirection = 1;
+				}
+			}
+			else
+			{
+				intendedVelocity.X *= 0.9f;
+				if (Math.Abs(intendedVelocity.X) < 0.5f && grounded)
+				{ // Player close to the ground and not moving = landing frame
+					if (intendedVelocity.Y < 0.25f)
+					{
+						intendedVelocity.Y = 0.25f;
+					}
+					intendedVelocity.X *= 0.5f;
+					anchor.Frame = 0;
+					Landed = true;
+				}
+			}
+
+			if (projectile.gfxOffY != 0 || owner.gfxOffY != 0)
+			{ // fuck slopes all my homies hate slopes
+				if (intendedVelocity.Y > -0.5f)
+				{
+					intendedVelocity.Y = -0.5f;
+				} 
+				projectile.gfxOffY = 0;
+				owner.gfxOffY = 0;
+			}
+
+			Vector2 finalVelocity = Vector2.Zero;
+			intendedVelocity /= 10f;
+			for (int i = 0; i < 10; i++)
+			{
+				finalVelocity += Collision.TileCollision(projectile.position + finalVelocity, intendedVelocity, projectile.width, projectile.height, false, false, (int)owner.gravDir);
+			}
+
+			projectile.velocity = finalVelocity;
+
+			anchor.OldPosition.Add(projectile.Center);
+			anchor.OldRotation.Add(projectile.rotation);
+			anchor.OldFrame.Add(anchor.Frame);
+
+			if (anchor.OldPosition.Count > 5)
+			{
+				anchor.OldPosition.RemoveAt(0);
+				anchor.OldRotation.RemoveAt(0);
+				anchor.OldFrame.RemoveAt(0);
+			}
+		}
+
+		/*
 		public override void ShapeshiftAnchorAI(Projectile projectile, ShapeshifterShapeshiftAnchor anchor)
 		{
 			Player owner = Main.player[projectile.owner];
@@ -148,5 +307,6 @@ namespace OrchidMod.Content.Shapeshifter.Weapons.Sage
 				anchor.OldFrame.RemoveAt(0);
 			}
 		}
+		*/
 	}
 }
