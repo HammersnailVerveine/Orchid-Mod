536c09e3dc57e5cbec41a8890819b434431333c3
Samuel Mounes <samuelpitch@hotmail.fr>
2024-10-17 23:39:25 +0200

    turtling rn

--- Content/Shapeshifter/Weapons/Warden/WardenTortoise.cs
+++ Content/Shapeshifter/Weapons/Warden/WardenTortoise.cs
@@ -49,19 +49,10 @@ namespace OrchidMod.Content.Shapeshifter.Weapons.Warden
 			TouchedGround = false;
 			LateralMovement = false;
 			CanAscend = false;
-
-			for (int i = 0; i < 8; i++)
-			{
-				FeatherDust(projectile, 1);
-			}
 		}
 
 		public override void OnKillAnchor(Projectile projectile, ShapeshifterShapeshiftAnchor anchor)
 		{
-			for (int i = 0; i < 5; i++)
-			{
-				FeatherDust(projectile, 1);
-			}
 		}
 
 		public override void ShapeshiftAnchorAI(Projectile projectile, ShapeshifterShapeshiftAnchor anchor, Player player, OrchidShapeshifter shapeshifter)
@@ -70,8 +61,28 @@ namespace OrchidMod.Content.Shapeshifter.Weapons.Warden
 
 			// ANIMATION
 
+			if (anchor.Projectile.ai[0] != 0)
+			{ // Override animation during left and right click attack
+				projectile.direction = (int)anchor.Projectile.ai[1];
+				projectile.spriteDirection = projectile.direction;
 
-			if (LateralMovement)
+				if (anchor.Projectile.ai[0] > 0)
+				{ // Left Click
+					anchor.Projectile.ai[0]--;
+					anchor.Frame = (anchor.Projectile.ai[0] > 5 ? 7 : 8);
+				}
+				else
+				{ // Right Click
+					anchor.Projectile.ai[0]++;
+					anchor.Frame = (anchor.Projectile.ai[0] < -5 && anchor.Projectile.ai[0] > -295 ? 6 : 5);
+				}
+
+				if (anchor.Projectile.ai[0] == 0)
+				{ // Puts the animation back on track
+					anchor.Frame = 0;
+				}
+			}
+			else if (LateralMovement)
 			{ // Player is moving left or right, cycle through frames
 				if (anchor.Timespent % 6 == 0 && anchor.Timespent > 0)
 				{
@@ -92,31 +103,38 @@ namespace OrchidMod.Content.Shapeshifter.Weapons.Warden
 
 			Vector2 intendedVelocity = projectile.velocity;
 
-			intendedVelocity.Y += 0.1f;
+			intendedVelocity.Y += 0.2f;
 
 			// Normal movement
 			if (player.controlLeft || player.controlRight)
 			{
-				if (player.controlLeft && !player.controlRight)
-				{ // Left movement
-					intendedVelocity.X -= 0.1f;
-					if (intendedVelocity.X < -2f) intendedVelocity.X = -2f;
-					projectile.direction = -1;
-					projectile.spriteDirection = -1;
-					LateralMovement = true;
-				}
-				else if (player.controlRight && !player.controlLeft)
-				{ // Right movement
-					intendedVelocity.X += 0.1f;
-					if (intendedVelocity.X > 2f) intendedVelocity.X = 2f;
-					projectile.direction = 1;
-					projectile.spriteDirection = 1;
-					LateralMovement = true;
+				if (projectile.ai[0] < -5)
+				{ // Prevents horizontal movement while blocking, but cancels the block
+					projectile.ai[0] = -5;
 				}
 				else
 				{
-					LateralMovement = false;
-					intendedVelocity.X *= 0.8f;
+					if (player.controlLeft && !player.controlRight)
+					{ // Left movement
+						intendedVelocity.X -= 0.1f;
+						if (intendedVelocity.X < -2f) intendedVelocity.X = -2f;
+						projectile.direction = -1;
+						projectile.spriteDirection = -1;
+						LateralMovement = true;
+					}
+					else if (player.controlRight && !player.controlLeft)
+					{ // Right movement
+						intendedVelocity.X += 0.1f;
+						if (intendedVelocity.X > 2f) intendedVelocity.X = 2f;
+						projectile.direction = 1;
+						projectile.spriteDirection = 1;
+						LateralMovement = true;
+					}
+					else
+					{
+						LateralMovement = false;
+						intendedVelocity.X *= 0.8f;
+					}
 				}
 			}
 			else
@@ -133,42 +151,32 @@ namespace OrchidMod.Content.Shapeshifter.Weapons.Warden
 			{
 				if (CanLeftClick(anchor) && !Landed)
 				{ // Left click attack
+					/*
 					int projectileType = ModContent.ProjectileType<SageOwlProj>();
-					for (int i = 0; i < 3; i++)
-					{
-						Vector2 velocity = Vector2.Normalize(Main.MouseWorld - projectile.Center).RotatedByRandom(MathHelper.ToRadians(7.5f)) * Item.shootSpeed * (0.85f + i * 0.15f);
-						int damage = shapeshifter.GetShapeshifterDamage(Item.damage);
-						Projectile newProjectile = Projectile.NewProjectileDirect(Item.GetSource_FromAI(), projectile.Center, velocity, projectileType, damage, Item.knockBack, player.whoAmI);
-						newProjectile.CritChance = shapeshifter.GetShapeshifterCrit(Item.crit);
-						newProjectile.netUpdate = true;
-					}
+					Vector2 velocity = Vector2.Normalize(Main.MouseWorld - projectile.Center).RotatedByRandom(MathHelper.ToRadians(7.5f)) * Item.shootSpeed;
+					int damage = shapeshifter.GetShapeshifterDamage(Item.damage);
+					Projectile newProjectile = Projectile.NewProjectileDirect(Item.GetSource_FromAI(), projectile.Center, velocity, projectileType, damage, Item.knockBack, player.whoAmI);
+					newProjectile.CritChance = shapeshifter.GetShapeshifterCrit(Item.crit);
+					newProjectile.netUpdate = true;
+					*/
 
 					anchor.LeftCLickCooldown = Item.useTime;
 					anchor.Projectile.ai[0] = 10;
 					anchor.Projectile.ai[1] = (Main.MouseWorld.X < projectile.Center.X ? -1f : 1f);
 					anchor.NeedNetUpdate = true;
 
+					anchor.Frame = 7;
 					SoundEngine.PlaySound(SoundID.DD2_MonkStaffSwing, projectile.Center);
-					FeatherDust(projectile, 2);
 				}
 
 				if (CanRightClick(anchor))
-				{ // Spawns a projectile that does nothing but provides visuals on the ability effect
-				}
-			}
-
-			if (anchor.Projectile.ai[0] > -30)
-			{ // Override animation during attack
-				anchor.Projectile.ai[0]--;
-				if (anchor.Projectile.ai[0] == 0)
-				{
-					anchor.Frame = 1;
-				}
-				else if (anchor.Projectile.ai[0] > 0)
-				{
-					anchor.Frame = 7;
-					projectile.direction = (int)anchor.Projectile.ai[1];
-					projectile.spriteDirection = projectile.direction;
+				{ // Right click attack
+					anchor.NeedNetUpdate = true;
+					anchor.RightCLickCooldown = Item.useTime;
+					anchor.Projectile.ai[0] = -300;
+					anchor.Projectile.ai[1] = (Main.MouseWorld.X < projectile.Center.X ? -1f : 1f);
+					projectile.velocity.X = 0f;
+					SoundEngine.PlaySound(SoundID.Item37, projectile.Center);
 				}
 			}
 
@@ -198,17 +206,6 @@ namespace OrchidMod.Content.Shapeshifter.Weapons.Warden
 
 				spriteBatch.End();
 				spriteBatch.Begin(spriteBatchSnapshot);
-			}
-		}
-
-		public void FeatherDust(Projectile projectile, int rand = 1)
-		{
-			if (Main.rand.NextBool(rand))
-			{
-				Dust dust = Dust.NewDustDirect(projectile.position, projectile.width, projectile.height, ModContent.DustType<SageOwlDust>(), Scale: Main.rand.NextFloat(1.2f, 1.4f));
-				dust.velocity *= 0.5f;
-				dust.velocity.Y = 2f;
-				dust.customData = Main.rand.Next(314);
 			}
 		}
 
