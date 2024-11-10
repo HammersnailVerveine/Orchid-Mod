665a1b082c461d5c6d5dd6f60015bcad86ffcd03
DefinitelyNotAmber <79143502+DefinitelyNotAmber@users.noreply.github.com>
2024-11-10 03:44:21 -0500

    Added Thorium Leaf Shield

---
+++ Content/Guardian/Projectiles/Shields/ThoriumLeafShieldProj.cs
@@ -0,0 +1,77 @@
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace OrchidMod.Content.Guardian.Projectiles.Shields
+{
+    public class ThoriumLeafShieldProj : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = 10;
+            Projectile.height = 10;
+            Projectile.timeLeft = 60;
+            Main.projFrames[Projectile.type] = 5;
+            DrawOffsetX = -2;
+            DrawOriginOffsetY = -4;
+            Projectile.frame = 0;
+            Projectile.tileCollide = false;
+            Projectile.friendly = true;
+            Projectile.penetrate = 2;
+        }
+
+        ref float dir => ref Projectile.ai[0];
+        ref float spin => ref Projectile.ai[1];
+
+        public override void AI()
+        {
+            if (spin != 0)
+            {
+                if (spin > 0) spin--; else spin++;
+                Projectile.rotation = dir + 1.571f + (float)Math.Pow(1 + spin * 0.25f, 2) * 0.2f;
+                Projectile.velocity *= 0.95f;
+                Projectile.velocity += new Vector2(1 / (1 + Math.Abs(spin)), 0).RotatedBy(dir);
+                if (Math.Abs(spin) <= 6)
+                {
+                    Projectile.frame = 1;
+                }
+            }
+            else
+            {
+                Projectile.tileCollide = true;
+                Projectile.velocity *= 0.99f;
+                Projectile.velocity += new Vector2(0.5f, 0).RotatedBy(dir);
+                Projectile.rotation = dir + 1.571f;
+                switch(Projectile.frameCounter)
+                {
+                    case 0:
+                        Projectile.frame = 2;
+                        break;
+                    case 5:
+                        Projectile.frame = 3;
+                        break;
+                    case 10:
+                        Projectile.frame = 4;
+                        break;
+                    case 15:
+                        goto case 5;
+                }
+                Projectile.frameCounter++;
+                if (Projectile.frameCounter >= 20) Projectile.frameCounter = 0;
+            }
+        }
+
+		public override void OnKill(int timeLeft)
+        {
+            Terraria.Audio.SoundEngine.PlaySound(Terraria.ID.SoundID.NPCHit11, Projectile.position);
+            for (int i = 0; i < 3; i++)
+            {
+                int dust = Dust.NewDust(Projectile.Center - new Vector2(4, 4), 0, 0, DustID.Grass);
+                Main.dust[dust].velocity = Projectile.velocity * 0.3f + Main.dust[dust].velocity * 0.5f;
+            }
+        }
+    }
+}
---
+++ Content/Guardian/Weapons/Shields/ThoriumLeafShield.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Xna.Framework;
+using OrchidMod.Content.Guardian.Projectiles.Shields;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace OrchidMod.Content.Guardian.Weapons.Shields
+{
+	public class ThoriumLeafShield : OrchidModGuardianShield
+	{
+
+		public override void SafeSetDefaults()
+		{
+			Item.width = 30;
+			Item.height = 34;
+			Item.rare = ItemRarityID.White;
+			Item.value = Item.sellPrice(0, 0, 0, 30);
+			Item.knockBack = 2.5f;
+			Item.damage = 22;
+			Item.useTime = 60;
+			distance = 30f;
+			slamDistance = 120f;
+			blockDuration = 80;
+			slamAutoReuse = false;
+			shouldFlip = true;
+		}
+
+		public override void AddRecipes()
+		{
+			var recipe = CreateRecipe();
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.AddIngredient(ItemID.Wood, 10);
+			recipe.Register();
+		}
+
+		private void LeafBurst(Player player, Projectile shield, int time, int spinTime = 30, int quantity = 3)
+		{
+            Terraria.Audio.SoundEngine.PlaySound(Terraria.ID.SoundID.Grass, shield.position);
+			float dir = (Main.MouseWorld - player.Center).ToRotation();
+			Vector2 spread = new Vector2(0, 1).RotatedBy(dir);
+			for (int i = 0; i < quantity; i++)
+			{
+				float side = (i - ((quantity - 1) / 2f)) * -shield.direction;
+				int leaf = Projectile.NewProjectile(Item.GetSource_FromThis(), shield.Center, spread.RotatedByRandom(Main.rand.NextFloat()) * side, ModContent.ProjectileType<ThoriumLeafShieldProj>(), (int)(shield.damage * 0.8f), Item.knockBack, player.whoAmI);
+				Main.projectile[leaf].ai[0] = dir + (side + Main.rand.NextFloat() - 0.5f) * 0.1f;
+				Main.projectile[leaf].ai[1] = spinTime * (Main.rand.NextBool() ? 1 : -1);
+				Main.projectile[leaf].timeLeft = time;
+                int dust = Dust.NewDust(shield.Center - new Vector2(4, 4), 0, 0, DustID.Grass);
+			}
+		}
+
+		public override void Protect(Player player, Projectile shield)
+		{
+			if (IsLocalPlayer(player))
+			{
+				LeafBurst(player, shield, 45, 51);
+				GuardianShieldAnchor anchor = shield.ModProjectile as GuardianShieldAnchor;
+				shield.ai[0] = 0;
+				anchor.NeedNetUpdate = true;
+			}
+		}
+
+		public override void SlamHitFirst(Player player, Projectile shield, NPC npc)
+		{
+			if (IsLocalPlayer(player))
+			{
+				LeafBurst(player, shield, 62 - (int)((player.Center - shield.Center).Length() / 120), 30);
+				GuardianShieldAnchor anchor = shield.ModProjectile as GuardianShieldAnchor;
+				shield.friendly = false;
+				shield.ai[1] = 0;
+				anchor.isSlamming = 0;
+				anchor.NeedNetUpdate = true;
+			}
+		}
+	}
+}
