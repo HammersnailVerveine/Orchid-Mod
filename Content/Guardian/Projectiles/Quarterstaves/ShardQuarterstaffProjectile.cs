88d7b7c140dbf27ef83834d5db6555891c03a65f
Samuel Mounes <samuelpitch@hotmail.fr>
2024-11-14 03:00:52 +0100

    Shard Quarterstaff progress

---
+++ Content/Guardian/Projectiles/Quarterstaves/ShardQuarterstaffProjectile.cs
@@ -0,0 +1,83 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using OrchidMod.Utilities;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.GameContent;
+
+namespace OrchidMod.Content.Guardian.Projectiles.Quarterstaves
+{
+	public class ShardQuarterstaffProjectile : OrchidModGuardianProjectile
+	{
+		public List<float> OldRotation;
+
+		public override void SafeSetDefaults()
+		{
+			Projectile.width = 90;
+			Projectile.height = 90;
+			Projectile.friendly = true;
+			Projectile.aiStyle = -1;
+			Projectile.timeLeft = 60;
+			Projectile.scale = 1f;
+			Projectile.penetrate = -1;
+			Projectile.tileCollide = false;
+			Projectile.alpha = 255;
+			Projectile.usesLocalNPCImmunity = true;
+			Projectile.localNPCHitCooldown = 40;
+			OldRotation = new List<float>();
+		}
+
+		public override void AI()
+		{
+			if (!initialized)
+			{
+				initialized = true;
+				Projectile.rotation = Projectile.ai[1];
+			}
+
+			Projectile.rotation += Projectile.ai[0];
+			if (Projectile.ai[0] > 0.25f)
+			{
+				Projectile.ai[0] *= 0.95f;
+				if (Projectile.ai[0] < 0.25f)
+				{
+					Projectile.ai[0] = 0.25f;
+				}
+			}
+
+			OldRotation.Add(Projectile.rotation);
+
+			if (OldRotation.Count > 10)
+			{
+				OldRotation.RemoveAt(0);
+			}
+		}
+
+		public override bool OrchidPreDraw(SpriteBatch spriteBatch, Microsoft.Xna.Framework.Color lightColor)
+		{
+		//	spriteBatch.End(out SpriteBatchSnapshot spriteBatchSnapshot);
+		//  spriteBatch.Begin(spriteBatchSnapshot with { BlendState = BlendState.Additive });
+
+			// Draw code here
+
+			Texture2D projTexture = TextureAssets.Projectile[Projectile.type].Value;
+			float colorMult = 1f;
+			if (Projectile.timeLeft < 20f) colorMult *= Projectile.timeLeft / 20f;
+
+			Vector2 drawPosition = Projectile.Center - Main.screenPosition;
+
+			for (int i = 0; i < OldRotation.Count; i++)
+			{
+				spriteBatch.Draw(projTexture, drawPosition, null, Color.White * 0.05f * (i + 1) * colorMult, OldRotation[i], projTexture.Size() * 0.5f, Projectile.scale * 1.25f, SpriteEffects.None, 0f);
+			}
+
+			spriteBatch.Draw(projTexture, drawPosition, null, Color.White * colorMult * 0.5f, Projectile.rotation, projTexture.Size() * 0.5f, Projectile.scale * 1.25f, SpriteEffects.None, 0f);
+
+			// Draw code ends here
+
+			//spriteBatch.End();
+			//spriteBatch.Begin(spriteBatchSnapshot);
+			return false;
+		}
+	}
+}
--- Content/Guardian/Weapons/Quarterstaves/ShardQuarterstaff.cs
+++ Content/Guardian/Weapons/Quarterstaves/ShardQuarterstaff.cs
@@ -1,7 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
-using System;
+using OrchidMod.Content.Guardian.Projectiles.Quarterstaves;
 using Terraria;
 using Terraria.ID;
+using Terraria.ModLoader;
 
 namespace OrchidMod.Content.Guardian.Weapons.Quarterstaves
 {
@@ -16,8 +17,19 @@ namespace OrchidMod.Content.Guardian.Weapons.Quarterstaves
 			Item.useTime = 30;
 			ParryDuration = 90;
 			Item.knockBack = 8f;
-			Item.damage = 282;
+			Item.damage = 172;
 			GuardStacks = 2;
+		}
+
+		public override void OnAttack(Player player, OrchidGuardian guardian, Projectile projectile, bool jabAttack, bool counterAttack)
+		{
+			if (counterAttack && IsLocalPlayer(player))
+			{
+				int damage = guardian.GetGuardianDamage(Item.damage * 0.5f);
+				int projectileType = ModContent.ProjectileType<ShardQuarterstaffProjectile>();
+				Projectile newProjectile = Projectile.NewProjectileDirect(Item.GetSource_FromAI(), player.Center, Vector2.Zero, projectileType, damage, 0f, projectile.owner, 0.8f, Main.rand.NextFloat(3.14f));
+				newProjectile.CritChance = guardian.GetGuardianCrit(Item.crit);
+			}
 		}
 	}
 }
