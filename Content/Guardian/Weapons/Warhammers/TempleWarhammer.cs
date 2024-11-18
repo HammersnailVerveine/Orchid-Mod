a6cf62985111ec75868ed03bd5813ceadfce9cad
Samuel Mounes <samuelpitch@hotmail.fr>
2024-11-18 16:23:59 +0100

    Temple warhammers gives 1 less guard charges

--- Content/Guardian/Weapons/Warhammers/TempleWarhammer.cs
+++ Content/Guardian/Weapons/Warhammers/TempleWarhammer.cs
@@ -20,7 +20,7 @@ namespace OrchidMod.Content.Guardian.Weapons.Warhammers
 			Item.damage = 243;
 			Item.useTime = 20;
 			Range = 45;
-			BlockStacks = 2;
+			BlockStacks = 1;
 			SlamStacks = 2;
 			ReturnSpeed = 1.5f;
 		}
