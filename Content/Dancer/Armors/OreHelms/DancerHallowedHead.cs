c2cf72b32cb0f0d01bd65187465bbbfed4100bc4
DefinitelyNotAmber <79143502+DefinitelyNotAmber@users.noreply.github.com>
2024-12-08 02:51:57 -0500

    ???

---
+++ Content/Dancer/Armors/OreHelms/DancerHallowedHead.cs
@@ -0,0 +1,58 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using OrchidMod.Common;
+using OrchidMod.Common.Attributes;
+
+namespace OrchidMod.Content.Dancer.Armors.OreHelms
+{
+	[AutoloadEquip(EquipType.Head)]
+	[ClassTag(ClassTags.Dancer)]
+	public class DancerHallowedHead : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			ArmorIDs.Head.Sets.DrawFullHair[Item.headSlot] = true;
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 22;
+			Item.height = 20;
+			Item.value = Item.sellPrice(0, 5, 0, 0);
+			Item.rare = ItemRarityID.Pink;
+			Item.defense = 8;
+		}
+
+		public override void UpdateEquip(Player player)
+		{
+			//
+		}
+
+		public override bool IsArmorSet(Item head, Item body, Item legs)
+		{
+			return body.type == ItemID.HallowedPlateMail && legs.type == ItemID.HallowedGreaves;
+		}
+
+		public override void UpdateArmorSet(Player player)
+		{
+			player.setBonus = Language.GetTextValue("ArmorSetBonus.Hallowed");
+			player.armorEffectDrawShadow = true;
+			player.onHitDodge = true;
+		}
+
+		public override void ArmorSetShadows(Player player)
+		{
+			player.armorEffectDrawOutlines = true;
+		}
+
+		/*public override void AddRecipes()
+		{
+			var recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.HallowedBar, 12);
+			recipe.AddTile(TileID.MythrilAnvil);
+			recipe.Register();
+		}*/
+	}
+}
