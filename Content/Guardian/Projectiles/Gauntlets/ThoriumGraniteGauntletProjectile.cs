2e5e250fcf9173f7cb94c4c7bb02ca755fca75a6
DefinitelyNotAmber <79143502+DefinitelyNotAmber@users.noreply.github.com>
2024-12-22 04:11:24 -0500

    Add Granite Feedbacker

---
+++ Content/Guardian/Projectiles/Gauntlets/ThoriumGraniteGauntletProjectile.cs
@@ -0,0 +1,125 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.GameContent;
+
+namespace OrchidMod.Content.Guardian.Projectiles.Gauntlets
+{
+	public class ThoriumGraniteGauntletProjectile : OrchidModGuardianProjectile
+	{
+		public override string Texture => $"Terraria/Images/Projectile_950";
+
+		public override void SafeSetDefaults()
+		{
+			Projectile.width = 10;
+			Projectile.height = 10;
+			Projectile.aiStyle = -1;
+			Projectile.timeLeft = 130;
+			Projectile.penetrate = -1;
+			Projectile.usesLocalNPCImmunity = true;
+			Projectile.localNPCHitCooldown = 30;
+			Projectile.hide = true;
+		}
+
+		public override void AI()
+		{
+			if (Projectile.timeLeft == 120)
+			{
+				Projectile.friendly = true;
+				if (Projectile.ai[0] == 1f)
+				{
+					Projectile.timeLeft = 20;
+				}
+				else
+				{
+					Projectile.velocity = Vector2.UnitY.RotatedBy((Main.MouseWorld - Main.player[Projectile.owner].MountedCenter).ToRotation() - MathHelper.PiOver2) * 8;
+					Projectile.extraUpdates = 89;
+					Projectile.numUpdates = 89;
+				}
+			}
+			else if (Projectile.timeLeft > 120)
+			{
+				Projectile.position += Main.player[Projectile.owner].position - Main.player[Projectile.owner].oldPosition;
+			}
+			else if (Projectile.timeLeft > 20)
+			{
+				Dust dust = Dust.NewDustPerfect(Projectile.position + new Vector2(5), DustID.TintableDustLighted, Vector2.Zero, Scale:1.2f - Projectile.timeLeft * 0.006f);
+				dust.alpha = 255;
+				dust.color = new Color(1f, 1f, 1f, 0f);
+				dust = Dust.NewDustPerfect((Projectile.position + Projectile.oldPosition) / 2f + new Vector2(5), DustID.TintableDustLighted, Vector2.Zero, Scale:1.2f - Projectile.timeLeft * 0.006f - 0.003f);
+				dust.alpha = 255;
+				dust.color = new Color(1f, 1f, 1f, 0f);
+			}
+			if (Projectile.timeLeft == 20)
+			{
+				Projectile.extraUpdates = 0;
+				Projectile.numUpdates = 0;
+				Projectile.tileCollide = false;
+				SoundEngine.PlaySound(SoundID.NPCDeath56.WithPitchOffset(0.3f), Projectile.Center);
+				for (int i = 0; i < 30; i++)
+				{
+					Dust dust = Dust.NewDustDirect(Projectile.Center, 0, 0, DustID.Electric);
+					if (i < 24) dust.velocity = dust.velocity * 3f - Projectile.velocity * 0.3f;
+					else
+					{
+						dust.velocity -= Projectile.velocity * 2;
+						dust.scale *= 0.8f;
+					}
+				}
+				Projectile.velocity = Vector2.Zero;
+			}
+		}
+
+		public override void SafeOnHitNPC(NPC target, NPC.HitInfo hit, int damageDone, Player player, OrchidGuardian guardian)
+		{
+			if (Projectile.timeLeft > 20)
+			{
+				Projectile.timeLeft = 21;
+			}
+		}
+
+		public override bool OnTileCollide(Vector2 oldVelocity)
+		{
+			Projectile.velocity = oldVelocity;
+			if (Projectile.timeLeft > 20)
+			{
+				Projectile.timeLeft = 21;
+			}
+			return false;
+		}
+
+		public override void DrawBehind(int index, List<int> behindNPCsAndTiles, List<int> behindNPCs, List<int> behindProjectiles, List<int> overPlayers, List<int> overWiresUI)
+		{
+			if (Projectile.timeLeft > 120) overPlayers.Add(index);
+			else behindProjectiles.Add(index);
+		}
+
+		public override bool OrchidPreDraw(SpriteBatch spriteBatch, Color lightColor)
+		{
+			if (Projectile.timeLeft > 120)
+			{
+				int flashTimeLeft = Projectile.timeLeft - 120;
+				Main.instance.LoadProjectile(79);
+				Main.EntitySpriteDraw(TextureAssets.Projectile[79].Value, Projectile.Center - Main.screenPosition, null, new Color(0.5f, 0.5f, 0.5f, 0f), Projectile.rotation, new Vector2(27), flashTimeLeft * 0.5f, SpriteEffects.None);
+			}
+			if (Projectile.timeLeft > 20) return false;
+			Main.EntitySpriteDraw(ModContent.Request<Texture2D>(Texture).Value, Projectile.Center - Main.screenPosition, null, new Color(0f, 0.5f, 1f, 0f) * (Projectile.timeLeft * 0.5f), 0, new Vector2(64), 2f - (Projectile.timeLeft * 0.1f), SpriteEffects.None);
+			return false;
+		}
+
+		public override void ModifyDamageHitbox(ref Rectangle hitbox)
+		{
+			if (Projectile.timeLeft <= 20)
+			{
+				hitbox.X -= 120;
+				hitbox.Y -= 120;
+				hitbox.Width += 240;
+				hitbox.Height += 240;
+			}
+		}
+	}
+}
---
+++ Content/Guardian/Weapons/Gauntlets/ThoriumGraniteGauntlet.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using OrchidMod.Common.ModObjects;
+using OrchidMod.Content.Guardian.Projectiles.Gauntlets;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace OrchidMod.Content.Guardian.Weapons.Gauntlets
+{
+	public class ThoriumGraniteGauntlet : OrchidModGuardianGauntlet
+	{
+		public override void SafeSetDefaults()
+		{
+			Item.width = 42;
+			Item.height = 38;
+			Item.knockBack = 10f;
+			Item.damage = 100;
+			Item.value = Item.sellPrice(0, 7, 50, 0);
+			Item.rare = ItemRarityID.Orange;
+			Item.useTime = 20;
+			strikeVelocity = 12f;
+			parryDuration = 60;
+			hasArm = true;
+			hasShoulder = true;
+		}
+
+		public override Color GetColor(bool offHand)
+		{
+			return new Color(0, 192, 255);
+		}
+
+		public override bool OnPunch(Player player, OrchidGuardian guardian, Projectile projectile, bool charged, ref int damage)
+		{
+			if (charged && guardian.UseGuard(1, true))
+			{
+				bool deflect = false;
+				bool instantExplode = true;
+				Vector2 strikeVelocity = Vector2.UnitY.RotatedBy((Main.MouseWorld - player.MountedCenter).ToRotation() - MathHelper.PiOver2) * 8;
+				Vector2 strikeEndPosition = projectile.Center + strikeVelocity * 10;
+				int punchDamage = guardian.GetGuardianDamage(Item.damage);
+				int highestDeflectedDamage = 0;
+				for (int i = 0; i < Main.maxProjectiles; i++)
+				{
+					Projectile deflectProj = Main.projectile[i];
+					if(deflectProj.active && deflectProj.hostile && deflectProj.damage > 0 && Collision.CheckAABBvLineCollision(deflectProj.position + deflectProj.velocity - new Vector2(16), new Vector2(deflectProj.width + 32, deflectProj.height + 32), projectile.Center, strikeEndPosition))
+					{
+						if (!deflect && guardian.UseGuard())
+						{
+							deflect = true;
+							guardian.OnBlockProjectileFirst(projectile, deflectProj);
+						}
+						if (deflect)
+						{
+							instantExplode = false;
+							guardian.OnBlockProjectile(projectile, deflectProj);
+							if (deflectProj.damage > highestDeflectedDamage) highestDeflectedDamage = deflectProj.damage;
+							deflectProj.Kill();
+						}
+					}
+				}
+				for (int i = 0; i < Main.maxNPCs; i++)
+				{
+					NPC deflectEnemy = Main.npc[i];
+					if (deflectEnemy.active && !deflectEnemy.friendly && Collision.CheckAABBvLineCollision(deflectEnemy.position + deflectEnemy.velocity - new Vector2(16), new Vector2(deflectEnemy.width + 32, deflectEnemy.height + 32), projectile.Center, strikeEndPosition) && (deflectEnemy.lifeMax < 2 || (deflectEnemy.velocity - player.velocity).Length() > 6f))
+					{
+						if (!deflect && guardian.UseGuard())
+						{
+							deflect = true;
+							guardian.OnBlockNPCFirst(projectile, deflectEnemy);
+						}
+						if (deflect)
+						{
+							guardian.OnBlockNPC(projectile, deflectEnemy);
+							if (deflectEnemy.damage > highestDeflectedDamage) highestDeflectedDamage = deflectEnemy.damage;
+							if (!deflectEnemy.dontTakeDamage)
+							{
+								NPC.HitInfo info = deflectEnemy.CalculateHitInfo(punchDamage, strikeVelocity.X > 1 ? 1 : -1, false, 1f, ModContent.GetInstance<GuardianDamageClass>());
+								if (info.Damage >= deflectEnemy.life) instantExplode = false; 
+								deflectEnemy.StrikeNPC(info);
+							}
+						}
+					}
+				}
+				if (deflect)
+				{
+					SoundEngine.PlaySound(SoundID.Item37.WithPitchOffset(0.5f), player.Center);
+					//SoundEngine.PlaySound(SoundID.Item106.WithPitchOffset(0.2f), player.Center);
+					//SoundEngine.PlaySound(SoundID.Item72, player.Center);
+					player.GetModPlayer<OrchidPlayer>().PlayerImmunity = player.immuneTime = InvincibilityDuration;
+					player.immune = true;
+					Projectile counterProj = Projectile.NewProjectileDirect(Item.GetSource_FromThis(), projectile.Center + strikeVelocity * 4, Vector2.Zero, ModContent.ProjectileType<ThoriumGraniteGauntletProjectile>(), Math.Clamp(highestDeflectedDamage, punchDamage, 1000), Item.knockBack, projectile.owner, instantExplode ? 1 : 0);
+					counterProj.rotation = Main.rand.NextFloat(MathHelper.TwoPi);
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+}
