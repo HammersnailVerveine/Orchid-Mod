def4ccea076a4c0deccd637dd46c52e355366596
DefinitelyNotAmber <79143502+DefinitelyNotAmber@users.noreply.github.com>
2024-12-29 15:00:12 -0500

    the funniest thing ever

---
+++ Content/Guardian/Weapons/Warhammers/ThoriumThorsHammerWarhammer.cs
@@ -0,0 +1,33 @@
+﻿using Terraria;
+using Terraria.ID;
+using OrchidMod.Common.ModObjects;
+
+namespace OrchidMod.Content.Guardian.Weapons.Warhammers
+{
+	public class ThoriumThorsHammerWarhammer : OrchidModGuardianHammer
+	{
+		public override void SafeSetDefaults()
+		{
+			Item.width = 34;
+			Item.height = 34;
+			Item.value = Item.sellPrice(0, 0, 54, 0);
+			Item.rare = ItemRarityID.Orange;
+			Item.UseSound = SoundID.DD2_MonkStaffSwing;
+			Item.knockBack = 5.5f;
+			Item.useTime = 18;
+			Item.shootSpeed = 16f;
+			Item.damage = 76;
+			Range = 22;
+			GuardStacks = 1;
+			ReturnSpeed = 1.5f;
+		}
+
+		public override void AddRecipes()
+		{
+			var recipe = CreateRecipe();
+			recipe.AddTile(TileID.Anvils);
+			recipe.AddIngredient(ItemID.HellstoneBar, 20);
+			recipe.Register();
+		}
+	}
+}
