6cff78ecf9d09b4c25fd07c2d7c1e9196e90a2f3
Alino4kaHvoshch <75527426+alinahvoshch@users.noreply.github.com>
2025-03-01 12:32:18 +0300

    Oops

--- Content/Alchemist/Misc/ReactionItem.cs
+++ Content/Alchemist/Misc/ReactionItem.cs
@@ -62,7 +62,7 @@ namespace OrchidMod.Content.Alchemist.Misc
 			Mod thoriumMod = OrchidMod.ThoriumMod;
 			if (thoriumMod != null)
 			{
-				tooltips.Insert(1, new TooltipLine(Mod, "ClassTag", Language.GetTextValue("Mods.OrchidMod.DamageClasses.AlchemistClassTag"))
+				tooltips.Insert(1, new TooltipLine(Mod, "ClassTag", Language.GetTextValue("Mods.OrchidMod.DamageClasses.Alchemist"))
 				{
 					OverrideColor = new Color(155, 255, 55)
 				});
