553ed59b250be64dabb509cea50a031838dce0e9
DefinitelyNotAmber <79143502+DefinitelyNotAmber@users.noreply.github.com>
2025-08-28 01:00:48 -0400

    Shield Surfing platform fix

--- Content/Guardian/Accessories/GuideShield.cs
+++ Content/Guardian/Accessories/GuideShield.cs
@@ -27,7 +27,7 @@ namespace OrchidMod.Content.Guardian.Accessories
 					if (anchor.aimedLocation.Y > player.Center.Y && (Math.Abs(anchor.aimedLocation.X - player.Center.X) < 32f) && anchor.Projectile.ai[0] > 0f)
 					{
 						// Collision with the ground, do skating stuff
-						Vector2 collision = Collision.TileCollision(player.position + new Vector2((player.width - Item.width) * 0.5f, player.height), Vector2.UnitY * 8f, Item.width, 16, true, true, (int)player.gravDir);
+						Vector2 collision = Collision.TileCollision(player.position + new Vector2((player.width - Item.width) * 0.5f, player.height), Vector2.UnitY * 8f, Item.width, 16, false, true, (int)player.gravDir);
 						if (collision != Vector2.UnitY * 8f)
 						{
 							player.fallStart = (int)(player.position.Y / 16f);
