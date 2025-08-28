c7feeb99f2a4220a16d7431510f929f8644b3c96
DefinitelyNotAmber <79143502+DefinitelyNotAmber@users.noreply.github.com>
2025-08-28 10:24:47 -0400

    Removed flat defense from BambooBuff

--- Content/Guardian/Buffs/BambooBuff.cs
+++ Content/Guardian/Buffs/BambooBuff.cs
@@ -14,7 +14,6 @@ namespace OrchidMod.Content.Guardian.Buffs
 
 		public override void Update(Player player, ref int buffIndex)
 		{
-			player.statDefense += 3;
 			player.statDefense *= 1.5f;
 		}
 	}
