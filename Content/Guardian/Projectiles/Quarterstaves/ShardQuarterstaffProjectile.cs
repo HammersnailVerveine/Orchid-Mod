d228728b2f0da8f7f352b8bc067882880d0946ae
DefinitelyNotAmber <79143502+DefinitelyNotAmber@users.noreply.github.com>
2025-10-22 05:09:31 -0400

    ShardQuarterstaff changes

--- Content/Guardian/Projectiles/Quarterstaves/ShardQuarterstaffProjectile.cs
+++ Content/Guardian/Projectiles/Quarterstaves/ShardQuarterstaffProjectile.cs
@@ -1,6 +1,7 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using OrchidMod.Utilities;
+using System;
 using System.Collections.Generic;
 using Terraria;
 using Terraria.GameContent;
@@ -9,6 +10,7 @@ namespace OrchidMod.Content.Guardian.Projectiles.Quarterstaves
 {
 	public class ShardQuarterstaffProjectile : OrchidModGuardianProjectile
 	{
+		public List<Vector2> OldPosition;
 		public List<float> OldRotation;
 
 		public override void SafeSetDefaults()
@@ -17,13 +19,14 @@ namespace OrchidMod.Content.Guardian.Projectiles.Quarterstaves
 			Projectile.height = 90;
 			Projectile.friendly = true;
 			Projectile.aiStyle = -1;
-			Projectile.timeLeft = 60;
+			Projectile.timeLeft = 90;
 			Projectile.scale = 1f;
 			Projectile.penetrate = -1;
 			Projectile.tileCollide = false;
 			Projectile.alpha = 255;
 			Projectile.usesLocalNPCImmunity = true;
-			Projectile.localNPCHitCooldown = 40;
+			Projectile.localNPCHitCooldown = 20;
+			OldPosition = new List<Vector2>();
 			OldRotation = new List<float>();
 		}
 
@@ -33,22 +36,37 @@ namespace OrchidMod.Content.Guardian.Projectiles.Quarterstaves
 			{
 				Initialized = true;
 				Projectile.rotation = Projectile.ai[1];
-			}
-
-			Projectile.rotation += Projectile.ai[0];
-			if (Projectile.ai[0] > 0.25f)
-			{
-				Projectile.ai[0] *= 0.95f;
-				if (Projectile.ai[0] < 0.25f)
+				if (Projectile.velocity.Length() >= 8)
 				{
-					Projectile.ai[0] = 0.25f;
+					Projectile.ai[2] = Projectile.velocity.Length() / 8 - 1;
 				}
 			}
 
+			//Projectile.velocity *= 0.99f;
+			Projectile.rotation += Projectile.ai[0];
+			if (Projectile.ai[0] > 0.2f)
+			{
+				Projectile.ai[0] *= 0.97f;
+				if (Projectile.ai[0] < 0.2f)
+				{
+					Projectile.ai[0] = 0.2f;
+				}
+			}
+
+			if (Projectile.ai[2] > 0 && Projectile.velocity != Vector2.Zero)
+			{
+				Projectile.velocity = Vector2.Lerp(Projectile.velocity * Projectile.ai[2] / Projectile.velocity.Length(), Projectile.velocity, 0.9f);
+			}
+			else Projectile.velocity *= 0.9f;
+
+			if (Projectile.timeLeft < 30) Projectile.scale = (float)Math.Sin(Projectile.timeLeft * 0.05f);
+
+			OldPosition.Add(Projectile.Center);
 			OldRotation.Add(Projectile.rotation);
 
 			if (OldRotation.Count > 10)
 			{
+				OldPosition.RemoveAt(0);
 				OldRotation.RemoveAt(0);
 			}
 		}
@@ -62,16 +80,15 @@ namespace OrchidMod.Content.Guardian.Projectiles.Quarterstaves
 
 			Texture2D projTexture = TextureAssets.Projectile[Projectile.type].Value;
 			float colorMult = 1f;
-			if (Projectile.timeLeft < 20f) colorMult *= Projectile.timeLeft / 20f;
-
-			Vector2 drawPosition = Projectile.Center - Main.screenPosition;
+			if (Projectile.timeLeft < 30f) colorMult *= Projectile.timeLeft / 30f;
 
 			for (int i = 0; i < OldRotation.Count; i++)
 			{
+				Vector2 drawPosition = OldPosition[i] - Main.screenPosition;
 				spriteBatch.Draw(projTexture, drawPosition, null, Color.White * 0.05f * (i + 1) * colorMult, OldRotation[i], projTexture.Size() * 0.5f, Projectile.scale * 1.25f, SpriteEffects.None, 0f);
 			}
 
-			spriteBatch.Draw(projTexture, drawPosition, null, Color.White * colorMult * 0.5f, Projectile.rotation, projTexture.Size() * 0.5f, Projectile.scale * 1.25f, SpriteEffects.None, 0f);
+			spriteBatch.Draw(projTexture, Projectile.Center - Main.screenPosition, null, Color.White * colorMult * 0.5f, Projectile.rotation, projTexture.Size() * 0.5f, Projectile.scale * 1.25f, SpriteEffects.None, 0f);
 
 			// Draw code ends here
 
--- Content/Guardian/Weapons/Quarterstaves/ShardQuarterstaff.cs
+++ Content/Guardian/Weapons/Quarterstaves/ShardQuarterstaff.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using OrchidMod.Common;
 using OrchidMod.Content.Guardian.Projectiles.Quarterstaves;
 using Terraria;
 using Terraria.ID;
@@ -18,7 +19,7 @@ namespace OrchidMod.Content.Guardian.Weapons.Quarterstaves
 			ParryDuration = 90;
 			Item.knockBack = 8f;
 			Item.damage = 192;
-			GuardStacks = 2;
+			GuardStacks = 1;
 			SlamStacks = 1;
 		}
 
@@ -26,16 +27,64 @@ namespace OrchidMod.Content.Guardian.Weapons.Quarterstaves
 		{
 			if (counterAttack && IsLocalPlayer(player))
 			{
-				int damage = guardian.GetGuardianDamage(Item.damage * 0.66f);
+				int damage = guardian.GetGuardianDamage(Item.damage * 0.25f);
 				int projectileType = ModContent.ProjectileType<ShardQuarterstaffProjectile>();
-				Projectile newProjectile = Projectile.NewProjectileDirect(Item.GetSource_FromAI(), projectile.Center, Vector2.Zero, projectileType, damage, 0f, projectile.owner, 0.5f, Main.rand.NextFloat(3.14f));
+				Projectile newProjectile = Projectile.NewProjectileDirect(Item.GetSource_FromAI(), projectile.Center, player.velocity, projectileType, damage, 0f, projectile.owner, 0.8f, Main.rand.NextFloat(3.14f));
 				newProjectile.CritChance = guardian.GetGuardianCrit(Item.crit);
 			}
 		}
 
 		public override void OnHit(Player player, OrchidGuardian guardian, NPC target, Projectile projectile, NPC.HitInfo hit, bool jabAttack, bool counterAttack)
 		{
-			if (Main.rand.NextBool() || !jabAttack) target.AddBuff(BuffID.Confused, 120 + Main.rand.Next(120));
+			if (counterAttack) target.AddBuff(BuffID.Confused, 60);
+			else
+			{
+				Vector2 pos = projectile.Center + new Vector2(28, -28).RotatedBy(projectile.rotation);
+				Vector2 vel = new Vector2(1, -1).RotatedBy(projectile.rotation);
+				Color color = Color.White with {A = 0};
+				bool whiteBlack = false;
+				if (!jabAttack && projectile.ModProjectile is GuardianQuarterstaffAnchor anchor)
+				{
+					if (anchor.DamageReset != 0)
+					{
+						whiteBlack = true;
+						vel = vel.RotatedBy(1.4f * player.direction);
+					}
+					else
+					{
+						vel = vel.RotatedBy(-1.4f * player.direction);
+					}
+				}
+				else whiteBlack = Main.rand.NextBool();
+				if (whiteBlack)
+				{
+					Dust dust = Dust.NewDustPerfect(pos, DustID.Wraith, vel, Alpha: 50, newColor: color, Scale: 2f);
+					dust.noGravity = true;
+					dust.fadeIn = 2f;
+					for (int i = 0; i < 15; i++)
+					{
+						dust = Dust.NewDustPerfect(pos, DustID.Snow, Alpha: 150, newColor: color, Scale: Main.rand.NextFloat(0.75f, 1.5f));
+						dust.velocity = dust.velocity * 1.5f + vel * Main.rand.NextFloat(5f);
+						dust.noGravity = true;
+						if (!jabAttack)
+							dust.fadeIn = Main.rand.NextFloat(1.5f);
+					}
+				}
+				else
+				{
+					Dust dust = Dust.NewDustPerfect(pos, DustID.Snow, vel, Alpha: 150, newColor: color, Scale: 2f);
+					dust.noGravity = true;
+					dust.fadeIn = 2f;
+					for (int i = 0; i < 15; i++)
+					{
+						dust = Dust.NewDustPerfect(pos, DustID.Wraith, Alpha: 50, newColor: color, Scale: Main.rand.NextFloat(0.75f, 1.5f));
+						dust.velocity = dust.velocity * 1.5f + vel * Main.rand.NextFloat(5f);
+						dust.noGravity = true;
+						if (!jabAttack)
+							dust.fadeIn = Main.rand.NextFloat(1.5f);
+					}
+				}
+			}
 		}
 
 		public override void AddRecipes()
