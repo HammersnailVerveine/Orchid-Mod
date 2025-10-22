f7c9662214557bc853722e4f9b9b486c60f5929c
DefinitelyNotAmber <79143502+DefinitelyNotAmber@users.noreply.github.com>
2025-10-22 19:50:55 -0400

    ThorsHammerWarhammer rework

--- Content/Guardian/Weapons/Warhammers/ThoriumThorsHammerWarhammer.cs
+++ Content/Guardian/Weapons/Warhammers/ThoriumThorsHammerWarhammer.cs
@@ -1,6 +1,8 @@
 ﻿using Terraria;
 using Terraria.ID;
 using OrchidMod.Common.Attributes;
+using Microsoft.Xna.Framework;
+using System;
 
 namespace OrchidMod.Content.Guardian.Weapons.Warhammers
 {
@@ -13,18 +15,19 @@ namespace OrchidMod.Content.Guardian.Weapons.Warhammers
 			Item.height = 34;
 			Item.value = Item.sellPrice(0, 0, 54, 0);
 			Item.rare = ItemRarityID.Orange;
-			Item.UseSound = SoundID.DD2_MonkStaffSwing;
-			Item.knockBack = 5.5f;
-			Item.useTime = 18;
-			Item.shootSpeed = 16f;
-			Item.damage = 142;
-			Range = 18;
+			Item.UseSound = SoundID.Item1;
+			Item.knockBack = 4f;
+			Item.useTime = 15;
+			Item.shootSpeed = 12f;
+			Item.damage = 134;
+			Range = 26;
 			GuardStacks = 1;
-			ReturnSpeed = 1.8f;
-			SwingSpeed = 1.5f;
-			HitCooldown = 20;
+			ReturnSpeed = 1.6f;
+			SwingChargeGain = 1.5f;
+			HitCooldown = 15;
+			BlockDamage = 0.2f;
 			Penetrate = true;
-			BlockDuration = 255;
+			BlockDuration = 80;
 		}
 
 		//public override bool CanRightClick() => true;
@@ -38,6 +41,59 @@ namespace OrchidMod.Content.Guardian.Weapons.Warhammers
 			}
 		}
 		*/
+
+		public override void OnThrow(Player player, OrchidGuardian guardian, Projectile projectile, bool Weak)
+		{
+			projectile.extraUpdates = 1;
+		}
+
+		/*public override bool ThrowAI(Player player, OrchidGuardian guardian, Projectile projectile, bool Weak)
+		{
+			Dust dust = Dust.NewDustDirect(projectile.position - new Vector2(4), projectile.width + 8, projectile.height + 8, DustID.MagicMirror, Alpha: 100, Scale: 0.8f);
+			dust.velocity = projectile.velocity * 0.1f + (dust.position - projectile.Center).RotatedBy(Math.Min(projectile.velocity.Length() * 0.1f, MathHelper.PiOver2) * projectile.velocity.X > 0 ? 1 : -1) * 0.2f;
+			if (Main.rand.NextBool(3))
+			{
+				dust.noGravity = true;
+				dust.velocity *= 2.5f;
+				dust.scale *= 1.5f;
+			}
+			return true;
+		}*/
+
+		public override void ExtraAI(Player player, OrchidGuardian guardian, Projectile projectile)
+		{
+			Dust dust = Dust.NewDustPerfect(projectile.Center, DustID.MagicMirror, Alpha: 100, Scale: 0.8f);
+			Vector2 offs = Main.rand.NextVector2Circular(10, 10);
+			dust.noGravity = true;
+			if ((projectile.ModProjectile as GuardianHammerAnchor).BlockDuration != 0)
+			{
+				dust.position += offs * 2;
+				dust.velocity = projectile.velocity * 0.2f + offs.RotatedBy(-MathHelper.PiOver2) * 0.5f;
+				dust.scale *= 1.5f;
+			}
+			else if (projectile.ai[1] <= 0)
+			{
+				//this line copied from ThoriumGrandThunderBirdWarhammer
+				//todo: make projectile.rotation actually work on warhammers so i don't have to do this for visuals
+				Vector2 gemPos = projectile.Center + new Vector2(8 * projectile.spriteDirection, -8).RotatedBy(projectile.ai[1] > 0 ? projectile.rotation : guardian.GuardianItemCharge * 0.0065f * player.gravDir * projectile.spriteDirection);
+				if (Main.rand.NextBool())
+				{
+					dust.scale *= 0.5f;
+					dust.fadeIn = Main.rand.NextFloat(1f);
+					offs *= 0.5f;
+				}
+				dust.position = gemPos + offs * 1.5f;
+				dust.velocity = offs * Main.rand.NextFloat(0.1f) + player.velocity * 0.33f;
+			}
+			else
+			{
+				dust.position += offs;
+				dust.velocity = projectile.velocity * 0.5f;
+				//dust.velocity = projectile.velocity * 0.2f + offs.RotatedBy(projectile.velocity.X > 0 ? MathHelper.PiOver2 : -MathHelper.PiOver2);
+				dust.scale *= Main.rand.NextFloat(0.6f, 1.6f);
+				dust.fadeIn = Main.rand.NextFloat(0.6f, 2.2f - dust.scale);
+			}
+		}
 
 		public override void AddRecipes()
 		{
