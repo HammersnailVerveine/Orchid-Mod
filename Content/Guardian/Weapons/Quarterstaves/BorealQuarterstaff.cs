84f51590b3b582a0110628c9e15f7e1ef0119dd1
Samuel Mounes <samuelpitch@hotmail.fr>
2025-12-18 19:40:34 +0100

    Boreal Quarterstaff & Mahogany Pavise

---
+++ Content/Guardian/Weapons/Quarterstaves/BorealQuarterstaff.cs
@@ -0,0 +1,29 @@
+﻿using Terraria;
+using Terraria.ID;
+
+namespace OrchidMod.Content.Guardian.Weapons.Quarterstaves
+{
+	public class BorealQuarterstaff : OrchidModGuardianQuarterstaff
+	{
+		public override void SafeSetDefaults()
+		{
+			Item.width = 42;
+			Item.height = 42;
+			Item.value = Item.sellPrice(0, 0, 0, 20);
+			Item.rare = ItemRarityID.White;
+			Item.useTime = 35;
+			ParryDuration = 48;
+			Item.knockBack = 8f;
+			Item.damage = 26;
+			GuardStacks = 1;
+		}
+
+		public override void AddRecipes()
+		{
+			var recipe = CreateRecipe();
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.AddIngredient(ItemID.BorealWood, 10);
+			recipe.Register();
+		}
+	}
+}
