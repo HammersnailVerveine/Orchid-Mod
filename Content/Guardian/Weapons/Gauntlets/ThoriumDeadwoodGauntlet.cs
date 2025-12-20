a7eef966e1222e4d91295bd0c15f8d2269d1ed9c
Samuel Mounes <samuelpitch@hotmail.fr>
2025-12-20 18:57:10 +0100

    Deadwood & Evergreen Wood Gauntlets

---
+++ Content/Guardian/Weapons/Gauntlets/ThoriumDeadwoodGauntlet.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+
+namespace OrchidMod.Content.Guardian.Weapons.Gauntlets
+{
+	public class ThoriumDeadwoodGauntlet : OrchidModGuardianGauntlet
+	{
+		public override void SafeSetDefaults()
+		{
+			Item.width = 30;
+			Item.height = 32;
+			Item.knockBack = 3f;
+			Item.damage = 27;
+			Item.value = Item.sellPrice(0, 0, 0, 20);
+			Item.rare = ItemRarityID.White;
+			Item.useTime = 20;
+			StrikeVelocity = 13f;
+			ParryDuration = 40;
+			PunchSpeed *= 1.15f;
+		}
+
+		public override Color GetColor(bool offHand)
+		{
+			if (offHand) return new Color(156, 172, 174);
+			return new Color(150, 45, 48);
+		}
+
+		public override void AddRecipes()
+		{
+			var thoriumMod = OrchidMod.ThoriumMod;
+			if (thoriumMod != null)
+			{
+				var recipe = CreateRecipe();
+				recipe.AddTile(TileID.WorkBenches);
+				recipe.AddIngredient(thoriumMod, "Deadwood", 18);
+				recipe.Register();
+			}
+		}
+	}
+}
---
+++ Content/Guardian/Weapons/Quarterstaves/ThoriumEvergreenWoodQuarterstaff.cs
@@ -0,0 +1,33 @@
+﻿using Terraria;
+using Terraria.ID;
+
+namespace OrchidMod.Content.Guardian.Weapons.Quarterstaves
+{
+	public class ThoriumEvergreenWoodQuarterstaff : OrchidModGuardianQuarterstaff
+	{
+		public override void SafeSetDefaults()
+		{
+			Item.width = 38;
+			Item.height = 38;
+			Item.value = Item.sellPrice(0, 0, 0, 20);
+			Item.rare = ItemRarityID.White;
+			Item.useTime = 30;
+			ParryDuration = 60;
+			Item.knockBack = 9f;
+			Item.damage = 241;
+			GuardStacks = 1;
+		}
+
+		public override void AddRecipes()
+		{
+			var thoriumMod = OrchidMod.ThoriumMod;
+			if (thoriumMod != null)
+			{
+				var recipe = CreateRecipe();
+				recipe.AddTile(TileID.WorkBenches);
+				recipe.AddIngredient(thoriumMod, "EvergreenBlock", 18);
+				recipe.Register();
+			}
+		}
+	}
+}
