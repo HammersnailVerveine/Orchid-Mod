3ae9e10205dd3f5807dcec498b23d14bec03e1e1
alinahvoshch <weterewe23@gmail.com>
2025-12-27 16:38:16 +0300

    fix to fix and missed ru line

--- Content/General/Prefixes/AccessoryPrefix.cs
+++ Content/General/Prefixes/AccessoryPrefix.cs
@@ -130,7 +130,7 @@ namespace OrchidMod.Content.General.Prefixes
 
 			if (alchemistPotency > 0)
 			{
-				tooltips.Insert(index, new TooltipLine(Mod, "AlchemistPotencyPrefix", Language.GetTextValue("Mods.OrchidMod.Effects.Prefixes.AddPotency", alchemistPotency))
+				tooltips.Insert(index, new TooltipLine(Mod, "AlchemistPotencyPrefix", Language.GetTextValue("Mods.OrchidMod.Prefixes.Effects.AddPotency", alchemistPotency))
 				{
 					IsModifier = true
 				});
